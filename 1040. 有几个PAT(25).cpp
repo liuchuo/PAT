6f730c503c839372d25a110f4ba83b35d529fc11
陈欣 <95323362@qq.com>
2016-01-06 14:16:45 +0800

    1040. 有几个PAT（25）

---
+++ 1040. 有几个PAT(25).cpp
@@ -0,0 +1,48 @@
+//1040. 有几个PAT（25）
+时间限制
+120 ms
+内存限制
+65536 kB
+代码长度限制
+8000 B
+判题程序
+Standard
+作者
+CAO, Peng
+字符串APPAPT中包含了两个单词“PAT”，其中第一个PAT是第2位(P),第4位(A),第6位(T)；
+第二个PAT是第3位(P),第4位(A),第6位(T)。
+现给定字符串，问一共可以形成多少个PAT？
+输入格式：
+输入只有一行，包含一个字符串，长度不超过105，只包含P、A、T三种字母。
+输出格式：
+在一行中输出给定字符串中包含多少个PAT。由于结果可能比较大，只输出对
+1000000007取余数的结果。
+输入样例：
+APPAPT
+输出样例：
+2
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int len = s.length();
+    int result = 0;
+    int countp = 0;
+    int countt = 0;
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 'T')
+            countt++;
+    }
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 'P') countp++;
+        if (s[i] == 'T') countt--;
+        if (s[i] == 'A') {
+            result = (result + (countp * countt) % 1000000007) % 1000000007;
+        }
+    }
+    cout << result;
+    return 0;
+}
