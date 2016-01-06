dbbb8be4a75ece0c73f66d92d397732b4e115c88
陈欣 <95323362@qq.com>
2016-01-06 14:07:33 +0800

    1032. 挖掘机技术哪家强(20)

---
+++ 1032. 挖掘机技术哪家强(20).cpp
@@ -0,0 +1,55 @@
+//1032. 挖掘机技术哪家强(20)
+为了用事实说明挖掘机技术到底哪家强，PAT组织了一场挖掘机技能大赛。
+现请你根据比赛结果统计出技术最强的那个学校。
+输入格式：
+输入在第1行给出不超过10^5的正整数N，即参赛人数。随后N行，每行给出一位参赛者的信息和成绩，
+包括其所代表的学校的编号（从1开始连续编号）、及其比赛成绩（百分制），中间以空格分隔。
+输出格式：
+在一行中给出总得分最高的学校的编号、及其总分，中间以空格分隔。题目保证答案唯一，没有并列。
+输入样例：
+6
+3 65
+2 80
+1 100
+2 70
+3 40
+3 0
+输出样例：
+2 150
+
+#include <iostream>
+using namespace std;
+int main() {
+    struct Student{
+        int num;
+        int score;
+    };
+    int N;
+    cin >> N;
+    Student *a = new Student [N];
+    for (int i = 0; i < N; i++) {
+        cin >> a[i].num;
+        cin >> a[i].score;
+    }
+    int *b = new int [N];
+    for (int i = 0; i < N; i++) {
+        b[i] = 0;
+    }
+    
+    for (int i = 0; i < N; i++) {
+        b[a[i].num] += a[i].score;
+    }
+    int max = b[1];
+    int t = 1;
+    for (int i = 1; i < N; i++) {
+        if (max < b[i]) {
+            max = b[i];
+            t = i;
+        }
+    }
+    cout << t << " " << max;
+    
+    delete [] a;
+    delete [] b;
+    return 0;
+}
