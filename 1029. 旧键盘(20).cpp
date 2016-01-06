f388eb758ff4699eea5907ceb3e20ccace98c583
陈欣 <95323362@qq.com>
2016-01-06 14:07:16 +0800

    1029. 旧键盘(20)

---
+++ 1029. 旧键盘(20).cpp
@@ -0,0 +1,67 @@
+//1029. 旧键盘(20)
+旧键盘上坏了几个键，于是在敲一段文字的时候，对应的字符就不会出现。
+现在给出应该输入的一段文字、以及实际被输入的文字，请你列出肯定坏掉
+的那些键。
+
+输入格式：
+
+输入在2行中分别给出应该输入的文字、以及实际被输入的文字。每段文字是
+不超过80个字符的串，由字母A-Z（包括大、小写）、数字0-9、以及下划线
+“_”（代表空格）组成。题目保证2个字符串均非空。
+
+输出格式：
+
+按照发现顺序，在一行中输出坏掉的键。其中英文字母只输出大写，每个坏键
+只输出一次。题目保证至少有1个坏键。
+
+输入样例：
+7_This_is_a_test
+_hs_s_a_es
+输出样例：
+7TI
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string a;
+    string b;
+    cin >> a;
+    cin >> b;
+    int lena = a.length();
+    int lenb = b.length();
+    char c[80];
+    int flag;
+    int t = 0;
+    for (int i = 0; i < lena; i++) {
+        flag = 0;
+        for (int j = 0; j < lenb; j++) {
+            if (a[i] == b[j]) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 0) {
+            if (a[i] >= 'a' && a[i] <= 'z') {
+                c[t++] = a[i] - 32;
+            }
+            else {
+                c[t++] = a[i];
+            }
+        }
+    }
+    
+    for (int i = 1; i < t; i++) {
+        for (int j = 0; j < i; j++) {
+            if (c[i] == c[j]) {
+                c[i] = '#';
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < t; i++) {
+        if (c[i] != '#')
+            cout << c[i];
+    }
+    return 0;
+}
