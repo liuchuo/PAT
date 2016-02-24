dbe97297f25d30dddd72e94f2723a6fb8b5eefa7
陈欣 <95323362@qq.com>
2016-02-24 21:29:48 +0800

    edit

--- 1025. 反转链表 (25).cpp
+++ 1025. 反转链表 (25).cpp
@@ -58,35 +58,35 @@ Address Data Next
 #include <iostream>
 using namespace std;
 int main() {
-	int first, k, n;
-	cin >> first >> n >> k;
-	int temp;
-	int data[100005];
-	int next[100005];
-	for (int i = 0; i < n; i++) {
-		cin >> temp;
-		cin >> data[temp];
-		cin >> next[temp];
-	}
-
-	int list[100005];
-	int sum = 0;//不一定所有的输入的结点都是有用的，加个计数器
-	while (first != -1) {
-		list[sum++] = first;
-		first = next[first];
-	}
-	int result[100005];
-	for (int i = 0; i < sum; i++) {
-		result[i] = list[i];
-	}
-
-	for (int i = 0; i < (sum - sum % k); i++) {
-		result[i] = list[i / k * k + k - 1 - i % k];
-	}
-
-	list[sum] = -1;
-
-	for (int i = 0; i < sum; i++)
-		printf("%05d %d %0d\n", result[i], data[result[i]], result[i + 1]);
-	return 0;
+    int first, k, n;
+    cin >> first >> n >> k;
+    int temp;
+    int data[100005];
+    int next[100005];
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        cin >> data[temp];
+        cin >> next[temp];
+    }
+    
+    int list[100005];
+    int sum = 0;//不一定所有的输入的结点都是有用的，加个计数器
+    while (first != -1) {
+        list[sum++] = first;
+        first = next[first];
+    }
+    int result[100005];
+    for (int i = 0; i < sum; i++) {
+        result[i] = list[i];
+    }
+    
+    for (int i = 0; i < (sum - sum % k); i++) {
+        result[i] = list[i / k * k + k - 1 - i % k];
+    }
+    
+    
+    for (int i = 0; i < sum - 1; i++)
+        printf("%05d %d %05d\n", result[i], data[result[i]], result[i + 1]);
+    printf("%05d %d -1", result[sum - 1], data[result[sum - 1]]);
+    return 0;
 }
