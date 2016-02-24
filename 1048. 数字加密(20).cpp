5d8ab348045b5f5608a169405602f01a9777e75a
陈欣 <95323362@qq.com>
2016-02-24 21:21:59 +0800

    EDIT

--- 1048. 数字加密(20).cpp
+++ 1048. 数字加密(20).cpp
@@ -45,15 +45,24 @@ int main() {
         b[i] = b[lenb - 1 - i];
         b[lenb - 1 - i] = d;
     }
+    
     int min = lena > lenb ? lenb : lena;
     int flag = lena > lenb ? 0 : 1;
     int max = lena > lenb ? lena : lenb;
     int temp;
-    for (int i = 0; i < min; i++) {
+    if (flag == 0) {
+        for (int i = 0; i < max - min; i++)
+            b += '0';
+    } else {
+        for (int i = 0; i < max - min; i++)
+            a += '0';
+    }
+    
+    for (int i = 0; i < max; i++) {
         if (i % 2 == 0) {
             temp = (a[i] - '0' + b[i] - '0') % 13;
             if(temp >= 0 && temp <= 9)
-                c += ((a[i] - '0' + b[i] - '0') % 13 + '0');
+                c += (temp + '0');
             else if (temp == 10)
                 c += 'J';
             else if (temp == 11)
@@ -67,15 +76,10 @@ int main() {
             c += temp + '0';
         }
     }
-    if (flag == 0) {
-        for (int i = min; i < lena; i++)
-            c += a[i];
-    } else {
-        for (int i = min; i < lenb; i++)
-            c += b[i];
-    }
+    
     for (int i = max - 1; i >= 0; i--) {
         cout << c[i];
     }
+    
     return 0;
 }
