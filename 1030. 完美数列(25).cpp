d7b412b788d3c6767a7e38751328e8d89c69e86c
陈欣 <95323362@qq.com>
2016-02-29 16:53:56 +0800

    add

---
+++ 1030. 完美数列(25).cpp
@@ -0,0 +1,66 @@
+1030. 完美数列(25)
+时间限制
+300 ms
+内存限制
+65536 kB
+代码长度限制
+8000 B
+判题程序
+Standard
+作者
+CAO, Peng
+给定一个正整数数列，和正整数p，设这个数列中的最大值是M，最小值是m，如果M <= m * p，则称这个数列是完美数列。
+
+现在给定参数p和一些正整数，请你从中选择尽可能多的数构成一个完美数列。
+
+输入格式：
+
+输入第一行给出两个正整数N和p，其中N（<= 105）是输入的正整数的个数，p（<= 109）是给定的参数。第二行给出N个正整数，每个数不超过109。
+
+输出格式：
+
+在一行中输出最多可以选择多少个数可以用它们组成一个完美数列。
+
+输入样例：
+10 8
+2 3 20 4 5 1 6 7 8 9
+输出样例：
+8
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int cmp(int a, int b) {
+    return a < b;
+}
+
+int main() {
+    int n;
+    long long p;
+    cin >> n >> p;
+    if (n == 0) {
+        cout << n;
+        return 0;
+    }
+    long long int *a = new long long int [n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a+n, cmp);
+    int result = 1;
+    int temp = 1;
+    for (int i = 0; i <= n - 2; i++) {
+        for (int j = i + result; j <= n - 1; j++) {
+            if (a[j] <= a[i] * p) {
+                temp = j - i + 1;
+                if (temp > result) {
+                    result = temp;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+    cout << result;
+    
+    return 0;
+}
