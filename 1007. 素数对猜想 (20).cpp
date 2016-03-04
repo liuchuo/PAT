c53af18aef5a8782349e037cb732c8d3696b45eb
陈欣 <95323362@qq.com>
2016-03-04 13:40:31 +0800

    update

--- 1007. 素数对猜想 (20).cpp
+++ 1007. 素数对猜想 (20).cpp
@@ -29,14 +29,11 @@ CHEN, Yue
 using namespace std;
 
 int isprime(int a) {
-    int flag = 1;
     for (int i = 2; i <= sqrt(a); i++) {
-        if (a % i == 0) {
-            flag = 0;
-            break;
-        }
+        if (a % i == 0)
+            return 0;
     }
-    return flag;
+    return 1;
 }
 
 int main() {
