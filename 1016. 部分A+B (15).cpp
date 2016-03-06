0a6dc4444e036d42e64ee3c2ae1f488d1a99e846
陈欣 <95323362@qq.com>
2016-03-06 11:37:52 +0800

    update

--- 1016. 部分A+B (15).cpp
+++ 1016. 部分A+B (15).cpp
@@ -38,30 +38,21 @@ int main() {
     string a, b;
     int m, n;
     cin >> a >> m >> b >> n;
-    int lena = a.length();
-    int lenb = b.length();
-    int counta = 0;
-    int countb = 0;
-    for (int i = 0; i < lena; i++) {
+    
+    int counta = 0, countb = 0;
+    for (int i = 0; i < a.length(); i++) {
         if (m == (a[i] - '0'))
             counta++;
     }
-    for (int i = 0; i < lenb; i++) {
+    for (int i = 0; i < b.length(); i++) {
         if (n == (b[i] - '0'))
             countb++;
     }
-    int ta = 0;
-    int tb = 0;
-    if (counta != 0) {
-        ta = m;
-    }
-    if (countb != 0) {
-        tb = n;
-    }
-    for (int i = 1; i < counta; i++) {
+    int ta = 0, tb = 0;
+    for (int i = 1; i <= counta; i++) {
         ta = 10 * ta + m;
     }
-    for (int i = 1; i < countb; i++) {
+    for (int i = 1; i <= countb; i++) {
         tb = 10 * tb + n;
     }
     cout << ta + tb;
