6c0675a062bf283166403e075fc4003d1791ed81
陈欣 <95323362@qq.com>
2016-03-06 11:46:11 +0800

    update

--- 1017. A除以B (20).cpp
+++ 1017. A除以B (20).cpp
@@ -34,19 +34,21 @@ int main() {
     cin >> s;
     int a;
     cin >> a;
+    
     int len = s.length();
-    int t = 0;
-    int temp = 0;
-    t = (s[0] - '0') / a;
+    int t = (s[0] - '0') / a;
     if ((t != 0 && len > 1) || len == 1) {
         cout << t;
     }
-    temp = (s[0] - '0') % a;
+
+    int temp = (s[0] - '0') % a;
+
     for (int i = 1; i < len; i++) {
         t = (temp * 10 + s[i] - '0') / a;
         cout << t;
         temp = (temp * 10 + s[i] - '0') % a;
     }
+    
     cout << " " << temp;
     return 0;
 }
