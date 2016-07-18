5154ccf876e9f1da31554d84bdf5b608389cfa85
陈欣 <95323362@qq.com>
2016-07-18 15:50:29 +0800

    -

---
+++ 1089. Insert or Merge (25).cpp
@@ -0,0 +1,83 @@
+1089. Insert or Merge (25)
+According to Wikipedia:
+
+Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list. Each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. It repeats until no input elements remain.
+
+Merge sort works as follows: Divide the unsorted list into N sublists, each containing 1 element (a list of 1 element is considered sorted). Then repeatedly merge two adjacent sublists to produce new sorted sublists until there is only 1 sublist remaining.
+
+Now given the initial sequence of integers, together with a sequence which is a result of several iterations of some sorting method, can you tell which sorting method we are using?
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line gives a positive integer N (<=100). Then in the next line, N integers are given as the initial sequence. The last line contains the partially sorted sequence of the N numbers. It is assumed that the target sequence is always ascending. All the numbers in a line are separated by a space.
+
+Output Specification:
+
+For each test case, print in the first line either "Insertion Sort" or "Merge Sort" to indicate the method used to obtain the partial result. Then run this method for one more iteration and output in the second line the resulting sequence. It is guaranteed that the answer is unique for each test case. All the numbers in a line must be separated by a space, and there must be no extra space at the end of the line.
+
+Sample Input 1:
+10
+3 1 2 8 7 5 9 4 6 0
+1 2 3 7 8 5 9 4 6 0
+Sample Output 1:
+Insertion Sort
+1 2 3 5 7 8 9 4 6 0
+Sample Input 2:
+10
+3 1 2 8 7 5 9 4 0 6
+1 3 2 8 5 7 4 9 0 6
+Sample Output 2:
+Merge Sort
+1 2 3 8 4 5 7 9 0 6
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int cmp(int a, int b) {
+    return a < b;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int *a = new int [n];
+    int *b = new int [n];
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> b[i];
+    }
+    
+    int i, j;
+    for (i = 0; i < n - 1 && b[i] <= b[i + 1]; i++);
+    for (j = i + 1; a[j] == b[j] && j < n; j++);
+    if (j == n) {
+        cout << "Insertion Sort" << endl;
+        sort(a, a + i + 2, cmp);
+    } else {
+        cout << "Merge Sort" << endl;
+        int k = 1;
+        int flag = 1;
+        while(flag) {
+            flag = 0;
+            for (i = 0; i < n; i++) {
+                if (a[i] != b[i])
+                    flag = 1;
+            }
+            k = k * 2;
+            for (i = 0; i < n / k; i++)
+                sort(a + i * k, a + (i + 1) * k, cmp);
+            for (i = n / k * k; i < n; i++)
+                sort(a + n / k * k, a + n, cmp);
+        }
+    }
+    
+    for (j = 0; j < n - 1; j++) {
+        cout << a[j] << " ";
+    }
+    cout << a[n - 1];
+    return 0;
+}
