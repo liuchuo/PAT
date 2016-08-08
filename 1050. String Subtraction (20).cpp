780121c27fa171181e13e973062faff3a34b307f
陈欣 <95323362@qq.com>
2016-08-08 16:00:07 +0800

    -

---
+++ 1050. String Subtraction (20).cpp
@@ -0,0 +1,40 @@
+1050. String Subtraction (20)
+Given two strings S1 and S2, S = S1 - S2 is defined to be the remaining string after taking all the characters in S2 from S1. Your task is simply to calculate S1 - S2 for any given strings. However, it might not be that simple to do it fast.
+
+Input Specification:
+
+Each input file contains one test case. Each case consists of two lines which gives S1 and S2, respectively. The string lengths of both strings are no more than 104. It is guaranteed that all the characters are visible ASCII codes and white space, and a new line character signals the end of a string.
+
+Output Specification:
+
+For each test case, print S1 - S2 in one line.
+
+Sample Input:
+They are students.
+aeiou
+Sample Output:
+Thy r stdnts.
+题目大意：给出两个字符串，在第一个字符串中删除第二个字符串中出现过的所有字符并输出。
+分析：用flag[256]数组变量标记str2出现过的字符为true，输出str1的时候根据flag[str1[i]]是否为true,如果是true就不输出
+
+#include <string>
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+using namespace std;
+int main() {
+    char s1[100000], s2[100000];
+    cin.getline(s1, 100000);
+    cin.getline(s2, 100000);
+    bool flag[256];
+    int lens1 = strlen(s1);
+    int lens2 = strlen(s2);
+    for(int i = 0; i < lens2; i++) {
+        flag[s2[i]] = true;
+    }
+    for(int i = 0; i < lens1; i++) {
+        if(!flag[s1[i]])
+            printf("%c", s1[i]);
+    }
+    return 0;
+}
---
+++ 1083. List Grades (25).cpp
@@ -0,0 +1,75 @@
+1083. List Grades (25)
+Given a list of N student records with name, ID and grade. You are supposed to sort the records with respect to the grade in non-increasing order, and output those student records of which the grades are in a given interval.
+
+Input Specification:
+
+Each input file contains one test case. Each case is given in the following format:
+
+N
+name[1] ID[1] grade[1]
+name[2] ID[2] grade[2]
+... ...
+name[N] ID[N] grade[N]
+grade1 grade2
+where name[i] and ID[i] are strings of no more than 10 characters with no space, grade[i] is an integer in [0, 100], grade1 and grade2 are the boundaries of the grade's interval. It is guaranteed that all the grades are distinct.
+
+Output Specification:
+
+For each test case you should output the student records of which the grades are in the given interval [grade1, grade2] and are in non-increasing order. Each student record occupies a line with the student's name and ID, separated by one space. If there is no student's grade in that interval, output "NONE" instead.
+
+Sample Input 1:
+4
+Tom CS000001 59
+Joe Math990112 89
+Mike CS991301 100
+Mary EE990830 95
+60 100
+Sample Output 1:
+Mike CS991301
+Mary EE990830
+Joe Math990112
+Sample Input 2:
+2
+Jean AA980920 60
+Ann CS01 80
+90 95
+Sample Output 2:
+NONE
+
+题目大意：给出n个考生的信息，按照分数从高到低排序，并且输出给定区间的考生信息。如果不存在满足条件的考生就输出NONE。
+分析：建立结构体数组，将不满足条件的学生grade改为-1，并统计满足条件的学生的个数cnt，然后进行排序，输出前cnt个考生的信息
+#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
+struct stu {
+    char name[12];
+    char id[12];
+    int grade;
+};
+bool cmp1(stu a, stu b) {
+    return a.grade > b.grade;
+}
+int main() {
+    int n, low, high, cnt = 0;
+    scanf("%d", &n);
+    vector<stu> v(n);
+    for(int i = 0; i < n; i++) {
+        scanf("%s %s %d", v[i].name, v[i].id, &v[i].grade);
+    }
+    scanf("%d %d", &low, &high);
+    for(int i = 0; i < n; i++) {
+        if(v[i].grade < low || v[i].grade > high) {
+            v[i].grade = -1;
+        } else {
+            cnt++;
+        }
+    }
+    sort(v.begin(), v.end(), cmp1);
+    for(int i = 0; i < cnt; i++) {
+        printf("%s %s\n", v[i].name, v[i].id);
+    }
+    if(cnt == 0)
+        printf("NONE");
+    return 0;
+}
