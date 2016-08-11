70cd4b3fe9506fed768ad0239f629d5d7c44ad27
陈欣 <95323362@qq.com>
2016-08-11 15:11:53 +0800

    -

---
+++ 1043. Is It a Binary Search Tree (25).cpp
@@ -0,0 +1,80 @@
+1043. Is It a Binary Search Tree (25)
+A Binary Search Tree (BST) is recursively defined as a binary tree which has the following properties:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than or equal to the node's key.
+Both the left and right subtrees must also be binary search trees.
+If we swap the left and right subtrees of every node, then the resulting tree is called the Mirror Image of a BST.
+
+Now given a sequence of integer keys, you are supposed to tell if it is the preorder traversal sequence of a BST or the mirror image of a BST.
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line contains a positive integer N (<=1000). Then N integer keys are given in the next line. All the numbers in a line are separated by a space.
+
+Output Specification:
+
+For each test case, first print in a line "YES" if the sequence is the preorder traversal sequence of a BST or the mirror image of a BST, or "NO" if not. Then if the answer is "YES", print in the next line the postorder traversal sequence of that tree. All the numbers in a line must be separated by a space, and there must be no extra space at the end of the line.
+
+Sample Input 1:
+7
+8 6 5 7 10 8 11
+Sample Output 1:
+YES
+5 7 6 8 11 10 8
+Sample Input 2:
+7
+8 10 11 8 6 7 5
+Sample Output 2:
+YES
+11 8 10 7 5 6 8
+Sample Input 3:
+7
+8 6 8 5 10 9 11
+Sample Output 3:
+NO
+题目大意：给定一个整数键值序列，现请你编写程序，判断这是否是对一棵二叉搜索树或其镜像进行前序遍历的结果。
+分析：假设它是二叉搜索树，一开始isMirror为FALSE，根据二叉搜索树的性质将已知的前序转换为后序，转换过程中，如果发现最后输出的后序数组长度不为n，那就设isMirror为true，然后清空后序数组，重新再转换一次（根据镜面二叉搜索树的性质），如果依旧转换后数组大小不等于n，就输出no否则输出yes
+
+#include <cstdio>
+#include <vector>
+using namespace std;
+bool isMirror;
+vector<int> pre;
+vector<int> post;
+void getpost(int root, int tail) {
+    if(root > tail) return ;
+    int i = root + 1, j = tail;
+    if(!isMirror) {
+        while(i <= tail && pre[root] > pre[i]) i++;
+        while(j > root && pre[root] <= pre[j]) j--;
+    } else {
+        while(i <= tail && pre[root] <= pre[i]) i++;
+        while(j > root && pre[root] > pre[j]) j--;
+    }
+    if(i - j != 1) return ;
+    getpost(root + 1, j);
+    getpost(i, tail);
+    post.push_back(pre[root]);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    pre.resize(n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &pre[i]);
+    getpost(0, n - 1);
+    if(post.size() != n) {
+        isMirror = true;
+        post.clear();
+        getpost(0, n - 1);
+    }
+    if(post.size() == n) {
+        printf("YES\n%d", post[0]);
+        for(int i = 1; i < n; i++)
+            printf(" %d", post[i]);
+    } else {
+        printf("NO");
+    }
+    return 0;
+}
