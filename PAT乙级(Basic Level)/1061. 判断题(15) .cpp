4e4f53785f189c865dfe7f80c9f05900094821ed
陈欣 <95323362@qq.com>
2017-01-01 17:42:11 +0800

    pat1061

---
+++ PAT乙级(Basic Level)/1061. 判断题(15) .cpp
@@ -0,0 +1,47 @@
+1061. 判断题(15)
+判断题的评判很简单，本题就要求你写个简单的程序帮助老师判题并统计学生们判断题的得分。
+
+输入格式：
+
+输入在第一行给出两个不超过100的正整数N和M，分别是学生人数和判断题数量。第二行给出M个不超过5的正整数，是每道题的满分值。第三行给出每道题对应的正确答案，0代表“非”，1代表“是”。随后N行，每行给出一个学生的解答。数字间均以空格分隔。
+
+输出格式：
+
+按照输入的顺序输出每个学生的得分，每个分数占一行。
+
+输入样例：
+3 6
+2 1 3 3 4 5
+0 0 1 0 1 1
+0 1 1 0 0 1
+1 0 1 0 1 0
+1 1 0 0 1 1
+输出样例：
+13
+11
+12
+
+
+#include <cstdio>
+#include <vector>
+using namespace std;
+int main() {
+    int n, m;
+    scanf("%d%d", &n, &m);
+    vector<int> score(m), ans(m);
+    for(int i = 0; i < m; i++)
+        scanf("%d", &score[i]);
+    for(int i = 0; i < m; i++)
+        scanf("%d", &ans[i]);
+    for(int i = 0; i < n; i++) {
+        int total = 0;
+        for(int j = 0; j < m; j++) {
+            int temp;
+            scanf("%d", &temp);
+            if(temp == ans[j])
+                total += score[j];
+        }
+        printf("%d\n", total);
+    }
+    return 0;
+}
---
+++ PAT乙级(Basic Level)/1062. 最简分数(20) .cpp
@@ -0,0 +1,46 @@
+1062. 最简分数(20)
+一个分数一般写成两个整数相除的形式：N/M，其中M不为0。最简分数是指分子和分母没有公约数的分数表示形式。
+
+现给定两个不相等的正分数 N1/M1 和 N2/M2，要求你按从小到大的顺序列出它们之间分母为K的最简分数。
+
+输入格式：
+
+输入在一行中按N/M的格式给出两个正分数，随后是一个正整数分母K，其间以空格分隔。题目保证给出的所有整数都不超过1000。
+
+输出格式：
+
+在一行中按N/M的格式列出两个给定分数之间分母为K的所有最简分数，按从小到大的顺序，其间以1个空格分隔。行首尾不得有多余空格。题目保证至少有1个输出。
+
+输入样例：
+7/18 13/20 12
+输出样例：
+5/12 7/12
+
+#include <cstdio>
+#include <iostream>
+using namespace std;
+int gcd(int a, int b){
+    return b == 0 ? a : gcd(b, a % b);
+}
+int main() {
+    int n1, m1, n2, m2, k;
+    scanf("%d/%d %d/%d %d", &n1, &m1, &n2, &m2, &k);
+    if(n1 * m2 > n2 * m1) {
+        swap(n1, n2);
+        swap(m1, m2);
+    }
+    int num = 1;
+    bool flag = false;
+    while(n1 * k >= m1 * num) num++;
+    while(n1 * k < m1 * num && m2 * num < n2 * k) {
+        if(gcd(num, k) == 1) {
+            if(!flag)
+                flag = true;
+            else
+                printf(" ");
+            printf("%d/%d", num, k);
+        }
+        num++;
+    }
+    return 0;
+}
