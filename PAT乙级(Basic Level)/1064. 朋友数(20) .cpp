ad6b6080ca9a0ce1aa709565f180c0263d44b221
liuchuo <95323362@qq.com>
2017-01-12 22:00:35 +0800

    -

---
+++ PAT乙级(Basic Level)/1064. 朋友数(20) .cpp
@@ -0,0 +1,45 @@
+1064. 朋友数(20)
+如果两个整数各位数字的和是一样的，则被称为是“朋友数”，而那个公共的和就是它们的“朋友证号”。例如123和51就是朋友数，因为1+2+3 = 5+1 = 6，而6就是它们的朋友证号。给定一些整数，要求你统计一下它们中有多少个不同的朋友证号。注意：我们默认一个整数自己是自己的朋友。
+
+输入格式：
+
+输入第一行给出正整数N。随后一行给出N个正整数，数字间以空格分隔。题目保证所有数字小于104。
+
+输出格式：
+
+首先第一行输出给定数字中不同的朋友证号的个数；随后一行按递增顺序输出这些朋友证号，数字间隔一个空格，且行末不得有多余空格。
+
+输入样例：
+8
+123 899 51 998 27 33 36 12
+输出样例：
+4
+3 6 9 26
+
+#include <cstdio>
+#include <set>
+using namespace std;
+int getFriendNum(int num) {
+    int sum = 0;
+    while(num != 0) {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+int main() {
+    set<int> s;
+    int n, num;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        s.insert(getFriendNum(num));
+    }
+    printf("%d\n", s.size());
+    for(set<int>::iterator it = s.begin(); it != s.end(); it++) {
+        if(it != s.begin())
+            printf(" ");
+        printf("%d", *it);
+    }
+    return 0;
+}
