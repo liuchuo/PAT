9134240f8b2e5644c2932e28e43cbe241a582d9e
liuchuo <95323362@qq.com>
2017-02-11 17:32:31 +0800

    -

--- PAT乙级(Basic Level)/1023. 组个最小数 (20).cpp
+++ PAT乙级(Basic Level)/1023. 组个最小数 (20).cpp
@@ -1,14 +1,4 @@
-//1023. 组个最小数 (20)
-时间限制
-100 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-CAO, Peng
+1023. 组个最小数 (20)
 给定数字0-9各若干个。你可以以任意顺序排列这些数字，但必须全部使用。目
 标是使得最后得到的数尽可能小（注意0不能做首位）。例如：给定两个0，
 两个1，三个5，一个8，我们得到的最小的数就是10015558。
@@ -32,9 +22,8 @@ using namespace std;
 
 int main() {
     int a[10];
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
         cin >> a[i];
-    }
     int t;
     for (int i = 1; i < 10; i++) {
         if (a[i] != 0) {
@@ -43,16 +32,12 @@ int main() {
             break;
         }
     }
-    for (int i = 0; i < a[0]; i++) {
+    for (int i = 0; i < a[0]; i++)
         cout << 0;
-    }
-    for (int i = 0; i < a[t] - 1; i++) {
+    for (int i = 0; i < a[t] - 1; i++)
         cout << t;
-    }
-    for (int i = t + 1; i < 10; i++) {
-        for (int k = 0; k < a[i]; k++) {
+    for (int i = t + 1; i < 10; i++)
+        for (int k = 0; k < a[i]; k++)
             cout << i;
-        }
-    }
     return 0;
 }
