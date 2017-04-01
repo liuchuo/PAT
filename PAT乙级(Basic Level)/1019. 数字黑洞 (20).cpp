e571e4cb7aa7e89a065bdb96c54a3594eb21cc9e
liuchuo <95323362@qq.com>
2017-04-01 17:16:04 +0800

    -

--- PAT乙级(Basic Level)/1019. 数字黑洞 (20).cpp
+++ PAT乙级(Basic Level)/1019. 数字黑洞 (20).cpp
@@ -37,52 +37,19 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-
-int cmp1(int a, int b) {
-    return a > b;
-}
-int cmp2(int a, int b) {
-    return a < b;
-}
-
+bool cmp(char a, char b) {return a > b;}
 int main() {
-    int N;
-    cin >> N;
-    int a[4] = {0};
-    int t = 3;
-    while (N != 0 && t != -1) {
-        a[t--] = N % 10;
-        N = N / 10;
-    }
-    t = 3;
-    if (a[0] == a[1] && a[1] == a[2] && a[2] == a[3]) {
-        cout << a[0] << a[0] << a[0] << a[0] << " - " << a[0] << a[0]
-             << a[0] << a[0] << " = " << "0000";
-        return 0;
-    }
-    int result = 0;
-    int b[4];
-    int temp;
-    while (result != 6174) {
-        for (int i = 0; i < 4; i++) {
-            b[i] = a[i];
-        }
-        sort(a, a + 4, cmp1);
-        sort(b, b + 4, cmp2);
-        result = a[0] * 1000 + a[1] * 100 + a[2] * 10 + a[3] -
-        b[0] * 1000 - b[1] * 100 - b[2] * 10 - b[3];
-        cout << a[0] << a[1] << a[2] << a[3] << " - " <<
-                b[0] << b[1] << b[2] << b[3] << " = ";
-        temp = result;
-        for (int i = 0; i < 4; i++) {
-            a[i] = 0;
-        }
-        while (temp != 0 && t != -1) {
-            a[t--] = temp % 10;
-            temp = temp / 10;
-        }
-        t = 3;
-        cout << a[0] << a[1] << a[2] << a[3] << endl;
-    }
+    string s;
+    cin >> s;
+    s.insert(0, 4 - s.length(), '0');
+    do {
+        string a = s, b = s;
+        sort(a.begin(), a.end(), cmp);
+        sort(b.begin(), b.end());
+        int result = stoi(a) - stoi(b);
+        s = to_string(result);
+        s.insert(0, 4 - s.length(), '0');
+        cout << a << " - " << b << " = " << s << endl;
+    } while (s != "6174" && s != "0000");
     return 0;
 }
