220c2d6851bc3afcc4bfb3ed6b477acb158f8e23
liuchuo <95323362@qq.com>
2017-09-19 10:01:21 +0800

    -

---
+++ PAT甲级(Advanced Level)/1133. Splitting A Linked List (25).cpp
@@ -0,0 +1,78 @@
+1133. Splitting A Linked List (25)
+Given a singly linked list, you are supposed to rearrange its elements so that all the negative values appear before all of the non-negatives, and all the values in [0, K] appear before all those greater than K. The order of the elements inside each class must not be changed. For example, given the list being 18→7→-4→0→5→-6→10→11→-2 and K being 10, you must output -4→-6→-2→7→0→5→10→18→11.
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line contains the address of the first node, a positive N (<= 105) which is the total number of nodes, and a positive K (<=1000). The address of a node is a 5-digit nonnegative integer, and NULL is represented by -1.
+
+Then N lines follow, each describes a node in the format:
+
+Address Data Next
+
+where Address is the position of the node, Data is an integer in [-105, 105], and Next is the position of the next node. It is guaranteed that the list is not empty.
+
+Output Specification:
+
+For each case, output in order (from beginning to the end of the list) the resulting linked list. Each node occupies a line, and is printed in the same format as in the input.
+
+Sample Input:
+00100 9 10
+23333 10 27777
+00000 0 99999
+00100 18 12309
+68237 -6 23333
+33218 -4 00000
+48652 -2 -1
+99999 5 68237
+27777 11 48652
+12309 7 33218
+Sample Output:
+33218 -4 68237
+68237 -6 48652
+48652 -2 12309
+12309 7 00000
+00000 0 99999
+99999 5 23333
+23333 10 00100
+00100 18 27777
+27777 11 -1
+
+#include <iostream>
+#include <vector>
+using namespace std;
+struct node {
+    int data, next;
+}list[100000];
+vector<int> v[3];
+int main() {
+    int start, n, k, a;
+    scanf("%d%d%d", &start, &n, &k);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a);
+        scanf("%d%d", &list[a].data, &list[a].next);
+    }
+    int p = start;
+    while(p != -1) {
+        int data = list[p].data;
+        if (data < 0)
+            v[0].push_back(p);
+        else if (data >= 0 && data <= k)
+            v[1].push_back(p);
+        else
+            v[2].push_back(p);
+        p = list[p].next;
+    }
+    int flag = 0;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < v[i].size(); j++) {
+            if (flag == 0) {
+                printf("%05d %d ", v[i][j], list[v[i][j]].data);
+                flag = 1;
+            } else {
+                printf("%05d\n%05d %d ", v[i][j], v[i][j], list[v[i][j]].data);
+            }
+        }
+    }
+    printf("-1");
+    return 0;
+}
