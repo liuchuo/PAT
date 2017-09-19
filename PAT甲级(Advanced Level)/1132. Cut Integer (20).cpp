ee1c5aea5df8686188ee2a7c27a336ea7ea2da36
liuchuo <95323362@qq.com>
2017-09-19 09:11:52 +0800

    -

---
+++ PAT甲级(Advanced Level)/1132. Cut Integer (20).cpp
@@ -0,0 +1,43 @@
+1132. Cut Integer (20)
+Cutting an integer means to cut a K digits long integer Z into two integers of (K/2) digits long integers A and B. For example, after cutting Z = 167334, we have A = 167 and B = 334. It is interesting to see that Z can be devided by the product of A and B, as 167334 / (167 x 334) = 3. Given an integer Z, you are supposed to test if it is such an integer.
+
+Input Specification:
+
+Each input file contains one test case. For each case, the first line gives a positive integer N (<= 20). Then N lines follow, each gives an integer Z (10<=Z<=231). It is guaranteed that the number of digits of Z is an even number.
+
+Output Specification:
+
+For each case, print a single line "Yes" if it is such a number, or "No" if not.
+
+Sample Input:
+3
+167334
+2333
+12345678
+Sample Output:
+Yes
+No
+No
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+    int n, num;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &num);
+        int len = 0, tempnum = num;
+        while (tempnum != 0) {
+            tempnum = tempnum / 10;
+            len++;
+        }
+        int d = pow(10, len / 2);
+        int a = num % d, b = num / d;
+        if (a * b != 0 && num % (a * b) == 0)
+            printf("Yes\n");
+        else
+            printf("No\n");
+    }
+    return 0;
+}
