7cc955314761ff2eb7bb3acbb6183997309df5d4
liuchuo <95323362@qq.com>
2018-01-17 14:21:23 +0800

    1002-basic

--- PAT乙级(Basic Level)/1002. 写出这个数 (20).cpp
+++ PAT乙级(Basic Level)/1002. 写出这个数 (20).cpp
@@ -21,92 +21,20 @@ CHEN, Yue
 输出样例：
 yi san wu
 
-update-v2.0:
-string是类,printf()只能打印基本类型。
-
-#include <iostream>
-using namespace std;
-int main() {
-    string chinese[10] = {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
-    string s;
-    cin >> s;
-    int len = s.length();
-    int sum = 0;
-    for (int i = 0; i < len; i++) {
-        sum = s[i] - '0' + sum;
-    }
-    int *a = new int [len];
-    int t = 0;
-    if (sum == 0) {
-        cout << chinese[0];
-        return 0;
-    }
-    while (sum != 0) {
-        a[t++] = sum % 10;
-        sum = sum / 10;
-    }
-    for (int i = t - 1; i >= 0; i--) {
-        cout << chinese[a[i]];
-        if(i != 0)
-            printf(" ");
-    }
-    delete [] a;
-    return 0;
-}
-
-
-//以前写的方法。现在想来太傻了。。
 #include <iostream>
 #include <string>
 using namespace std;
 int main() {
     string s;
     cin >> s;
-    int len = s.length();
-    int *a = new int [len];
     int sum = 0;
-    for (int i = 0; i < len; i++) {
-        a[i] = s[i] - '0';
-        sum = sum + a[i];
+    string str[10] =  {"ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"};
+    for (int i = 0; i < s.length(); i++)
+        sum += (s[i] - '0');
+    string num = to_string(sum);
+    for (int i = 0; i < num.length(); i++) {
+        if (i != 0) cout << " ";
+        cout << str[num[i] - '0'];
     }
-    int *b = new int [len];
-    int j = 0;
-    if (sum == 0) {
-        cout << "ling";
-    }
-    else {
-        while (sum != 0) {
-            b[j++] = sum % 10;
-            sum = sum / 10;
-        }
-    }
-    for (int i = j - 1;i >= 1; i--) {
-        switch(b[i]) {
-            case 1: cout << "yi ";break;
-            case 2: cout << "er ";break;
-            case 3: cout << "san ";break;
-            case 4: cout << "si ";break;
-            case 5: cout << "wu ";break;
-            case 6: cout << "liu ";break;
-            case 7: cout << "qi ";break;
-            case 8: cout << "ba ";break;
-            case 9: cout << "jiu ";break;
-            case 0: cout << "ling ";break;
-        }
-    }
-    switch(b[0]) {
-        case 1: cout << "yi";break;
-        case 2: cout << "er";break;
-        case 3: cout << "san";break;
-        case 4: cout << "si";break;
-        case 5: cout << "wu";break;
-        case 6: cout << "liu";break;
-        case 7: cout << "qi";break;
-        case 8: cout << "ba";break;
-        case 9: cout << "jiu";break;
-        case 0: cout << "ling";break;
-    }
-    delete [] a;
-    delete [] b;
     return 0;
 }
