99ac87a883bcc63128de208215a4283f493dd9d0
liuchuo <95323362@qq.com>
2018-01-20 18:33:56 +0800

    :zap: Improving performance.

--- PAT乙级(Basic Level)/1011. A+B和C (15).cpp
+++ PAT乙级(Basic Level)/1011. A+B和C (15).cpp
@@ -1,24 +1,9 @@
-//1011. A+B和C (15)
-时间限制
-100 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-HOU, Qiming
+1011. A+B和C (15)
 给定区间[-2^31, 2^31]内的3个整数A、B和C，请判断A+B是否大于C。
-
 输入格式：
-
 输入第1行给出正整数T(<=10)，是测试用例的个数。随后给出T组测试用例，每组占一行，顺序给出A、B和C。整数间以空格分隔。
-
 输出格式：
-
 对每组测试用例，在一行中输出“Case #X: true”如果A+B>C，否则输出“Case #X: false”，其中X是测试用例的编号（从1开始）。
-
 输入样例：
 4
 1 2 3
@@ -33,20 +18,13 @@ Case #4: false
 
 #include <iostream>
 using namespace std;
-
 int main() {
     int n;
-    cin >> n;
+    scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         long long int a, b, c;
-        cin >> a >> b >> c;
-        cout << "Case #" << i + 1 << ": ";
-        if (a + b > c) {
-            cout << "true";
-        } else {
-            cout << "false";
-        }
-        cout << endl;
+        scanf("%lld%lld%lld", &a, &b, &c);
+        printf("Case #%d: %s\n", i + 1, a + b > c ? "true" : "false");
     }
     return 0;
 }
