5cc24ae4145b3bfe100c573aa52153747734aae1
liuchuo <95323362@qq.com>
2018-01-20 18:33:06 +0800

    :art: Improve format of the code.

--- PAT乙级(Basic Level)/1007. 素数对猜想 (20).cpp
+++ PAT乙级(Basic Level)/1007. 素数对猜想 (20).cpp
@@ -1,22 +1,8 @@
 1007. 素数对猜想 (20)
-时间限制
-400 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-CHEN, Yue
 让我们定义 dn 为：dn = pn+1 - pn，其中 pi 是第i个素数。显然有 d1=1 且对于n>1有 dn 是偶数。“素数对猜想”认为“存在无穷多对相邻且差为2的素数”。
-
 现给定任意正整数N (< 105)，请计算不超过N的满足猜想的素数对的个数。
-
 输入格式：每个测试输入包含1个测试用例，给出正整数N。
-
 输出格式：每个测试用例的输出占一行，不超过N的满足猜想的素数对的个数。
-
 输入样例：
 20
 输出样例：
@@ -24,23 +10,16 @@ CHEN, Yue
 
 #include <iostream>
 using namespace std;
-
 bool isprime(int a) {
-    for (int i = 2; i * i <= a; i++) {
-        if (a % i == 0)
-            return false;
-    }
+    for (int i = 2; i * i <= a; i++)
+        if (a % i == 0) return false;
     return true;
 }
-
 int main() {
-    int N;
+    int N, cnt = 0;
     cin >> N;
-    int cnt = 0;
-    for (int i = 5; i <= N; i++) {
-        if (isprime(i-2) && isprime(i))
-            cnt++;
-    }
+    for (int i = 5; i <= N; i++)
+        if (isprime(i-2) && isprime(i)) cnt++;
     cout << cnt;
     return 0;
 }
