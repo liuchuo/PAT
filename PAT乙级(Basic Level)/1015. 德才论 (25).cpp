0a3b25035c714576811b543448b48e34b7d2b205
liuchuo <95323362@qq.com>
2018-01-23 00:10:07 +0800

    :art: Improve structure of the code.

--- PAT乙级(Basic Level)/1015. 德才论 (25).cpp
+++ PAT乙级(Basic Level)/1015. 德才论 (25).cpp
@@ -1,28 +1,11 @@
 1015. 德才论 (25)
-时间限制
-200 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-CHEN, Li
 宋代史学家司马光在《资治通鉴》中有一段著名的“德才论”：“是故才德全尽谓之圣人，才德兼亡谓之愚人，德胜才谓之君子，才胜德谓之小人。凡取人之术，苟不得圣人，君子而与之，与其得小人，不若得愚人。”
-
 现给出一批考生的德才分数，请根据司马光的理论给出录取排名。
-
 输入格式：
-
 输入第1行给出3个正整数，分别为：N（<=105），即考生总数；L（>=60），为录取最低分数线，即德分和才分均不低于L的考生才有资格被考虑录取；H（<100），为优先录取线——德分和才分均不低于此线的被定义为“才德全尽”，此类考生按德才总分从高到低排序；才分不到但德分到线的一类考生属于“德胜才”，也按总分排序，但排在第一类考生之后；德才分均低于H，但是德分不低于才分的考生属于“才德兼亡”但尚有“德胜才”者，按总分排序，但排在第二类考生之后；其他达到最低线L的考生也按总分排序，但排在第三类考生之后。
-
 随后N行，每行给出一位考生的信息，包括：准考证号、德分、才分，其中准考证号为8位整数，德才分为区间[0, 100]内的整数。数字间以空格分隔。
-
 输出格式：
-
 输出第1行首先给出达到最低分数线的考生人数M，随后M行，每行按照输入格式输出一位考生的信息，考生按输入中说明的规则从高到低排序。当某类考生中有多人总分相同时，按其德分降序排列；若德分也并列，则按准考证号的升序输出。
-
 输入样例：
 14 60 80
 10000001 64 90
@@ -54,78 +37,46 @@ CHEN, Li
 10000008 75 79
 10000001 64 90
 
-
+分析：用结构体存储。写好cmp函数~结构体数组vector v[4]中v[0]保存第一类考生，v[1]保存第二类考生……以此类推。写好cmp函数很重要，cmp函数中，排序先按照总分排序，然后按照德分排序，最后按照才分排序……最后输出符合条件的结果～
 #include <iostream>
 #include <algorithm>
-#include <cstdio>
 #include <vector>
 using namespace std;
-
 struct node {
-    int num;
-    int de;
-    int cai;
+    int num, de, cai;
 };
-
 int cmp(struct node a, struct node b) {
-    if ((a.de + a.cai) != (b.de + b.cai)) {
+    if ((a.de + a.cai) != (b.de + b.cai))
         return (a.de + a.cai) > (b.de + b.cai);
-    }else if (a.de != b.de) {
+    else if (a.de != b.de)
         return a.de > b.de;
-    } else
+    else
         return a.num < b.num;
 }
-
 int main() {
     int n, low, high;
-    cin >> n >> low >> high;
-    vector<node> a, b, c, d;
+    scanf("%d %d %d", &n, &low, &high);
+    vector<node> v[4];
     node temp;
     int total = n;
     for (int i = 0; i < n; i++) {
-        cin >> temp.num >> temp.de >> temp.cai;
-        if (temp.de < low || temp.cai < low) {
+        scanf("%d %d %d", &temp.num, &temp.de, &temp.cai);
+        if (temp.de < low || temp.cai < low)
             total--;
-            continue;
-        }
-        if (temp.de >= high && temp.cai >= high) {
-                a.push_back(temp);
-            } else if (temp.de >= high && temp.cai < high) {
-                b.push_back(temp);
-            } else if (temp.de < high && temp.cai < high && temp.de >= temp.cai) {
-                c.push_back(temp);
-            } else {
-                d.push_back(temp);
-            }
+        else if (temp.de >= high && temp.cai >= high)
+            v[0].push_back(temp);
+        else if (temp.de >= high && temp.cai < high)
+            v[1].push_back(temp);
+        else if (temp.de < high && temp.cai < high && temp.de >= temp.cai)
+            v[2].push_back(temp);
+        else
+            v[3].push_back(temp);
     }
-    
-    sort(a.begin(), a.end(), cmp);
-    sort(b.begin(), b.end(), cmp);
-    sort(c.begin(), c.end(), cmp);
-    sort(d.begin(), d.end(), cmp);
-    
     printf("%d\n", total);
-    vector<node>::iterator itr;
-    for(itr = a.begin(); itr != a.end(); itr++)
-        printf("%d %d %d\n", itr->num, itr->de, itr->cai);
-    
-    for(itr = b.begin(); itr != b.end(); itr++)
-        printf("%d %d %d\n", itr->num, itr->de, itr->cai);
-    
-    for(itr = c.begin(); itr != c.end(); itr++)
-        printf("%d %d %d\n", itr->num, itr->de, itr->cai);
-    
-    for(itr = d.begin(); itr != d.end(); itr++)
-        printf("%d %d %d\n", itr->num, itr->de, itr->cai);
-
+    for (int i = 0; i < 4; i++) {
+        sort(v[i].begin(), v[i].end(), cmp);
+        for (int j = 0; j < v[i].size(); j++)
+            printf("%d %d %d\n", v[i][j].num, v[i][j].de, v[i][j].cai);
+    }
     return 0;
 }
-
-
-
-
-
-
-
-
-
