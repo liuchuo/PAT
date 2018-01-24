b72ac55120274d6192316707525823ebd17130a0
liuchuo <95323362@qq.com>
2018-01-24 15:45:11 +0800

    :art: Improve format of the code.

--- PAT乙级(Basic Level)/1020. 月饼 (25).cpp
+++ PAT乙级(Basic Level)/1020. 月饼 (25).cpp
@@ -29,9 +29,7 @@
 #include <vector>
 using namespace std;
 struct mooncake{
-    float mount;
-    float price;
-    float unit;
+    float mount, price, unit;
 };
 int cmp(mooncake a, mooncake b) {
     return a.unit > b.unit;
@@ -40,12 +38,9 @@ int main() {
     int n, need;
     cin >> n >> need;
     vector<mooncake> a(n);
-    for (int i = 0; i < n; i++)
-        cin >> a[i].mount;
-    for (int i = 0; i < n; i++)
-        cin >> a[i].price;
-    for (int i = 0; i < n; i++)
-        a[i].unit = a[i].price / a[i].mount;
+    for (int i = 0; i < n; i++) scanf("%f", &a[i].mount);
+    for (int i = 0; i < n; i++) scanf("%f", &a[i].price);
+    for (int i = 0; i < n; i++) a[i].unit = a[i].price / a[i].mount;
     sort(a.begin(), a.end(), cmp);
     float result = 0.0;
     for (int i = 0; i < n; i++) {
