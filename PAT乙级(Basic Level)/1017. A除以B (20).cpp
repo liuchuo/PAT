9af5fc4c501a5e539d0d47d4561efd7550aa4032
liuchuo <95323362@qq.com>
2018-01-24 15:44:29 +0800

    :art: Improve format of the code

--- PAT乙级(Basic Level)/1017. A除以B (20).cpp
+++ PAT乙级(Basic Level)/1017. A除以B (20).cpp
@@ -1,14 +1,4 @@
-//1017. A除以B (20)
-时间限制
-100 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-CHEN, Yue
+1017. A除以B (20)
 本题要求计算A/B，其中A是不超过1000位的正整数，B是1位正整数。你需要输出商数Q和余数R，
 使得A = B * Q + R成立。
 
@@ -26,29 +16,21 @@ CHEN, Yue
 17636684150141093474 3
 
 #include <iostream>
-#include <string>
 using namespace std;
-
 int main() {
     string s;
-    cin >> s;
-    int a;
-    cin >> a;
-    
+    int a, t = 0, temp = 0;
+    cin >> s >> a;
     int len = s.length();
-    int t = (s[0] - '0') / a;
-    if ((t != 0 && len > 1) || len == 1) {
+    t = (s[0] - '0') / a;
+    if ((t != 0 && len > 1) || len == 1)
         cout << t;
-    }
-
-    int temp = (s[0] - '0') % a;
-
+    temp = (s[0] - '0') % a;
     for (int i = 1; i < len; i++) {
         t = (temp * 10 + s[i] - '0') / a;
         cout << t;
         temp = (temp * 10 + s[i] - '0') % a;
     }
-    
     cout << " " << temp;
     return 0;
 }
