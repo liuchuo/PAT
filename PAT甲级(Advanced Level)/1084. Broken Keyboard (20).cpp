5c0ab3825a7deed93983ceb847b5ae4379ee1d9a
liuchuo <95323362@qq.com>
2018-02-05 15:39:03 +0800

    :hammer: Refactoring code with string.find() and toupper

--- PAT甲级(Advanced Level)/1084. Broken Keyboard (20).cpp
+++ PAT甲级(Advanced Level)/1084. Broken Keyboard (20).cpp
@@ -18,48 +18,14 @@ Sample Output:
 7TI
 
 #include <iostream>
+#include <cctype>
 using namespace std;
 int main() {
-    string a;
-    cin >> a;
-    int lena = a.length();
-    string b;
-    cin >> b;
-    int lenb = b.length();
-    char c[100];
-    int i = 0, j = 0, t = 0;
-    
-    while (j <= lenb - 1) {
-        if (b[j] != a[i]) {
-            c[t++] = a[i];
-            i++;
-        } else {
-            i++;
-            j++;
-        }
-    }
-    
-    if (i != lena) {
-        for (int k = i; k <= lena - 1; k++) {
-            c[t++] = a[k];
-        }
-    }
-    
-    for (int k = 0; k < t; k++) { if (c[k] >= 'a' && c[k] <= 'z')
-        c[k] = c[k] - 32;
-    }
-    
-    for (int k = 0; k < t; k++) {
-        for (int m = 0; m < k; m++) {
-            if (c[m] == c[k]) {
-                c[k] = '#';
-            }
-        }
-    }
-    
-    for (int k = 0; k < t; k++) {
-        if (c[k] != '#')
-            cout << c[k];
-    }
+    string s1, s2, ans;
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.length(); i++)
+        if (s2.find(s1[i]) == string::npos && ans.find(toupper(s1[i])) == string::npos)
+            ans += toupper(s1[i]);
+    cout << ans;
     return 0;
 }
