712af63f6303daea896ecce23ca1353057d3bf7b
liuchuo <95323362@qq.com>
2018-02-10 02:45:34 +0800

    :bug: catch a bug~biu~biu~biu~

--- PAT乙级(Basic Level)/1051. 复数乘法 (15).cpp
+++ PAT乙级(Basic Level)/1051. 复数乘法 (15).cpp
@@ -1,26 +1,10 @@
 1051. 复数乘法 (15)
-时间限制
-400 ms
-内存限制
-65536 kB
-代码长度限制
-8000 B
-判题程序
-Standard
-作者
-CHEN, Yue
 复数可以写成(A + Bi)的常规形式，其中A是实部，B是虚部，i是虚数单位，满足i2 = -1；也可以写成极坐标下的指数形式(R*e(Pi))，其中R是复数模，P是辐角，i是虚数单位，其等价于三角形式(R(cos(P) + isin(P))。
-
 现给定两个复数的R和P，要求输出两数乘积的常规形式。
-
 输入格式：
-
 输入在一行中依次给出两个复数的R1, P1, R2, P2，数字间以空格分隔。
-
 输出格式：
-
 在一行中按照“A+Bi”的格式输出两数乘积的常规形式，实部和虚部均保留2位小数。注意：如果B是负数，则应该写成“A-|B|i”的形式。
-
 输入样例：
 2.3 3.5 5.2 0.4
 输出样例：
@@ -28,21 +12,19 @@ CHEN, Yue
 
 #include <iostream>
 #include <cmath>
-#include <cstdio>
 using namespace std;
 int main() {
-    double r1, p1, r2, p2;
+    double r1, p1, r2, p2, A, B;
     cin >> r1 >> p1 >> r2 >> p2;
-    double A, B;
     A = r1 * r2 * cos(p1) * cos(p2) - r1 * r2 * sin(p1) * sin(p2);
     B = r1 * r2 * cos(p1) * sin(p2) + r1 * r2 * sin(p1) * cos(p2);
-    if (A + 0.01 >= 0 && A < 0)
+    if (A + 0.005 >= 0 && A < 0)
         printf("0.00");
     else
         printf("%.2f", A);
     if(B >= 0)
         printf("+%.2fi", B);
-    else if (B + 0.01 >= 0 && B < 0)
+    else if (B + 0.005 >= 0 && B < 0)
         printf("+0.00i");
     else
         printf("%.2fi", B);
