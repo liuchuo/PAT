767a5c740b9b97833121908abdd5baff05141a05
liuchuo <95323362@qq.com>
2018-02-15 21:44:20 +0800

    :art: Improve format of the code

--- PAT乙级(Basic Level)/1031. 查验身份证(15).cpp
+++ PAT乙级(Basic Level)/1031. 查验身份证(15).cpp
@@ -47,10 +47,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         string a;
         cin >> a;
-        if (func(a))
-            count++;
+        if (func(a)) count++;
     }
-    if (count == 0)
+    if (count == 0) 
         cout << "All passed";
     return 0;
 }
@@ -58,17 +57,15 @@ int main() {
 bool func(string s) {
     int sum = 0;
     int a[18];
-    for (int i = 0; i < 17; i++) {
+    for (int i = 0; i < 17; i++)
         a[i] = s[i] - '0';
-    }
     if (s[17] == 'X')
         a[17] = 10;
     else
         a[17] = s[17] - '0';
     int b[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
-    for (int i = 0; i < 17; i++) {
+    for (int i = 0; i < 17; i++)
         sum = sum + a[i] * b[i];
-    }
     sum = sum % 11;
     int c[11] = {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2};
     if (c[sum] != a[17]) {
