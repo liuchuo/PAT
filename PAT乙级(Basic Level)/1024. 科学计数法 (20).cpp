3769dbc00e26e1f9c5512c841e81b160d0590c47
liuchuo <95323362@qq.com>
2018-03-01 23:11:10 +0800

    :hammer: Refactoring the code with liuchuo's lovely brain

--- PAT乙级(Basic Level)/1024. 科学计数法 (20).cpp
+++ PAT乙级(Basic Level)/1024. 科学计数法 (20).cpp
@@ -14,73 +14,32 @@
 输出样例2：
 -12000000000
 
-#include <iostream>
-#include <string>
-using namespace std;
+分析：n保存E后面的字符串所对应的数字，t保存E前面的字符串，不包括符号位。当n<0时表示向前移动，那么先输出0. 然后输出abs(n)-1个0，然后继续输出t中的所有数字；当n>0时候表示向后移动，那么先输出第一个字符，然后将t中尽可能输出n个字符，如果t已经输出到最后一个字符(j == t.length())那么就在后面补n-cnt个0，否则就补充一个小数点. 然后继续输出t剩余的没有输出的字符～
 
+#include <iostream>
+using namespace std;
 int main() {
     string s;
     cin >> s;
-    int len = s.length();
-    // 输出第1位符号
-    if (s[0] == '-')
-        cout << "-";
-    // 找到E的位置，标记为pose = i;
-    int pose = 0;
-    for (int i = 1; i < len; i++) {
-        if (s[i] == 'E')
-            pose = i;
-    }
-    // 把E后面的字符串数转化为after的数值
-    int after = 0;
-    for (int i = pose + 2; i < len; i++) {
-        after = (s[i] - '0') + 10 * after;
-    }
-    // 如果E后面的那个符号是负号‘-’
-    if (s[pose + 1] == '-') {
-        if (0 < after) {
-            cout << "0.";
-            for (int i = 1; i < after; i++) {
-                cout << 0;
-            }
-            for (int i = 1; i < pose; i++) {
-                if (s[i] >= '0' && s[i] <= '9') {
-                    cout << s[i];
-                }
-            }
+    int i = 0;
+    while (s[i] != 'E') i++;
+    string t = s.substr(1, i-1);
+    int n = stoi(s.substr(i+1));
+    if (s[0] == '-') cout << "-";
+    if (n < 0) {
+        cout << "0.";
+        for (int j = 0; j < abs(n) - 1; j++) cout << '0';
+        for (int j = 0; j < t.length(); j++)
+            if (t[j] != '.') cout << t[j];
+    } else {
+        cout << t[0];
+        int cnt, j;
+        for (j = 2, cnt = 0; j < t.length() && cnt < n; j++, cnt++) cout << t[j];
+        if (j == t.length()) {
+            for (int k = 0; k < n - cnt; k++) cout << '0';
         } else {
-            for (int i = 0; i < pose; i++) {
-                cout << s[i];
-            }
-        }
-    }
-    // 如果E后面的那个符号是‘+’
-    else {
-        // 如果要在后面添加0
-        if (pose - 3 < after) {
-            if (s[1] != '0')
-                cout << s[1];
-            for (int i = 3; i < pose; i++) {
-                if (s[i] >= '0' && s[i] <= '9') {
-                    cout << s[i];
-                }
-            }
-            for (int i = 0; i < after - (pose - 3); i++) {
-                cout << 0;
-            }
-        }
-        // 如果不用再后面添加0
-        else {
-            if (s[1] != '0')
-                cout << s[1];
-            for (int i = 3; i < pose; i++) {
-                if (i == 3 + after) {
-                    cout << ".";
-                }
-                if (s[i] >= '0' && s[i] <= '9') {
-                    cout << s[i];
-                }
-            }
+            cout << '.';
+            for (int k = j; k < t.length(); k++) cout << t[k];
         }
     }
     return 0;
