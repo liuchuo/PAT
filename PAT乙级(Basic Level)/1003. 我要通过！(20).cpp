9fa92556b98d97e84ad199f0facbd11b43608300
liuchuo <95323362@qq.com>
2018-03-01 22:21:35 +0800

    :hammer: Refactoring code with liuchuo's clever brain

--- PAT乙级(Basic Level)/1003. 我要通过！(20).cpp
+++ PAT乙级(Basic Level)/1003. 我要通过！(20).cpp
@@ -27,77 +27,43 @@ NO
 NO
 NO
 
+分析：
+任意形如 xPATx 的字符串都可以获得“答案正确”，其中 x 或者是空字符串，或者是仅由字母 A 组成的字符串；
+那么正确的有这些：
+PAT
+APATA
+AAPATAA
+AAAPATAAA
+...不说了，就是中间一个A左右加上等量的A（不加也行）都是正确的。
+
+如果 aPbTc 是正确的，那么 aPbATca 也是正确的，其中 a, b, c 均或者是空字符串，或者是仅由字母 A 组成的字符串。
+拿上面的那几个正确的举例子，那么正确的有这些：
+PAT —— 对于 aPbTc 来说ac是空，b是A。所以 PAAT 是正确的。同理PAAAAAT中间加多少个A都是正确哒~
+APATA —— 对于aPbTc来说，abc都是A。所以 APAATAA 是正确的。再类推一下，那么 APAAATAAA 是正确的。
+AAPATAA —— 对于aPbTc来说，a和c是AA，b是A。所以AAPAATAAAA是正确的，再类推一下，AAPAAATAAAAAA 是正确的~ 
+所以说规律就是，可以在P和T中间加A并且在T后面加A，要求必须是，中间加上一个A，末尾就得加上几倍的(P前面A的那个字符串)。换句话说就是，中间的A的个数如果是3，那么末尾的A的个数就得是开头A的个数的3倍。很巧，当中间A为一个的时候，末尾和开头A的个数必须相等正好是第二条的要求~~~
+
+所以一句话总结字符串的要求：只能有一个P一个T，中间末尾和开头可以随便插入A。但是必须满足开头的A的个数 * 中间的A的个数 = 结尾的A的个数
+
 #include <iostream>
-#include <string>
+#include <map>
 using namespace std;
-
-int func(string str);
-
 int main() {
-    int n;
+    int n, p = 0, t = 0;
+    string s;
     cin >> n;
-    getchar(); //读入n后要用getchar读走空格
-    string *s = new string [n];
-    for (int i = 0; i < n; i++) {
-        getline(cin, s[i]); //读入一整行的字符串，用getline(cin, string x);
-    }
-    int *t = new int [n];
-    for (int i = 0; i < n; i++) {
-        t[i] = 0;
-    }
-    for (int j = 0; j < n; j++) {
-        t[j] = func(s[j]);
-    }
-    
-    for (int k = 0; k < n; k++) {
-        if (t[k] == 1) {
-            cout << "YES" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> s;
+        map<char, int> m;
+        for(int j = 0; j < s.size(); j++) {
+            m[s[j]]++;
+            if (s[j] == 'P') p = j;
+            if (s[j] == 'T') t = j;
         }
-        if (t[k] == 0) {
-            cout << "NO" << endl;
-        }
+        if(m['P'] == 1 && m['A'] != 0 && m['T'] == 1 && m.size() == 3 && p * (t-p-1) == s.length()-t-1)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
-    delete [] s;
-    delete [] t;
     return 0;
-}
-
-int func(string str) {
-    int len = str.length();
-    if (len <= 2)
-        return 0;
-    for (int i = 0; i < len; i++) {
-        if (str[i] != 'P' && str[i] != 'A' && str[i] != 'T') {
-            return 0;
-        }
-        if (str[i] == 'P') {
-            // 找到P之后，P前面的所有字母只能是A，如果不是A，就return 0;
-            for (int j = 0; j < i; j++) {
-                if (str[j] != 'A')
-                    return 0;
-            }
-            // P之后的一个字母也必然是A，如果不是，直接return 0;
-            if (str [i + 1] != 'A')
-                return 0;
-
-            for (int k = i + 2; k < len; k++) {
-                if (str[k] != 'A') {
-                    // P后面若干个A，如果A结束后的第一个字母不是T，不符合直接return 0;
-                    if (str[k] != 'T') {
-                        return 0;
-                    }
-                    // k是当前T的位置 如果T后面的字符长度不是P前面字符长度的 k - 1 - i倍，错误直接return 0;
-                    if ((len - 1 - k) != ((k - 1 - i) * i))
-                        return 0;
-                    // 如果T后面的字母有不是A的。直接return 0;
-                    for (int m = k + 1; m < len; m++) {
-                        if (str[m] != 'A')
-                            return 0;
-                    }
-                }
-            }
-        }
-    }
-    // 如果有幸到了这一步，那就是正确的啦。return 1;
-    return 1;
 }
