241cc8d4bf84419adfbd560079f57f93006102f8
liuchuo <95323362@qq.com>
2018-03-24 20:52:40 +0800

    :art: add comment

--- PAT甲级(Advanced Level)/1064. Complete Binary Search Tree (30).cpp
+++ PAT甲级(Advanced Level)/1064. Complete Binary Search Tree (30).cpp
@@ -28,14 +28,14 @@ Sample Output:
 #include <algorithm>
 #include <cmath>
 using namespace std;
-#define min(a, b) ((a) < (b) ? (a) : (b))
 vector<int> in, level;
 void levelorder(int start, int end, int index) {
     if(start > end) return ;
     int n = end - start + 1;
-    int l =log(n + 1) / log(2);
-    int leave = n - (pow(2, l) - 1);
-    int root = start + pow(2, l - 1) - 1 + min(pow(2, l - 1), leave);
+    int n = end - start + 1;
+    int l = log(n + 1) / log(2); // 除了最后一层的层数
+    int leave = n - (pow(2, l) - 1);// 最后一层的叶子节点数
+    int root = start + (pow(2, l - 1) - 1) + min(pow(2, l - 1), leave); // pow(2, l - 1) - 1是除了root结点所在层和最后一层外，左子树的结点个数，pow(2, l - 1) 是l+1层最多拥有的属于根结点左子树的结点个数，min(pow(2, l - 1), leave)是最后一个结点真正拥有的属于根结点左子树上的结点个数
     level[index] = in[root];
     levelorder(start, root - 1, 2 * index + 1);
     levelorder(root + 1, end, 2 * index + 2);
