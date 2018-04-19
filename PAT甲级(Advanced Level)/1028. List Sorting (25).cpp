37530436b1c934749636af29ecdb57b9aa8eea7d
liuchuo <95323362@qq.com>
2018-04-19 22:38:25 +0800

    :bug: catch a bug in pat a 1028

--- PAT甲级(Advanced Level)/1028. List Sorting (25).cpp
+++ PAT甲级(Advanced Level)/1028. List Sorting (25).cpp
@@ -45,18 +45,20 @@ Sample Output 3
 using namespace std;
 const int maxn = 100001;
 struct NODE {
-    int no;
+    int no, score;
     char name[10];
-    int score;
 }node[maxn];
 int c;
 int cmp1(NODE a, NODE b) {
-    if(c == 1)
+    if(c == 1) {
         return a.no < b.no;
-    else if(c == 2)
+    } else if(c == 2) {
+        if(strcmp(a.name, b.name) == 0) return a.no < b.no;
         return strcmp(a.name, b.name) <= 0;
-    else
+    } else {
+        if(a.score == b.score) return a.no < b.no;
         return a.score <= b.score;
+    }
 }
 int main() {
     int n;
