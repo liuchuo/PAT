f5974198516d8dba6a5bbd5246110dc5e0b02080
liuchuo <95323362@qq.com>
2018-05-06 21:08:41 +0800

    :bug: I catch a little bug. It is a indiscoverable bug～

--- PAT乙级(Basic Level)/1010. 一元多项式求导 (25).cpp
+++ PAT乙级(Basic Level)/1010. 一元多项式求导 (25).cpp
@@ -10,21 +10,22 @@
 输出样例：
 12 3 -10 1 6 0
 
-分析：1.flag用来判断是否已经有过输出～当没有输出并且b==0的时候，输出“0 0”
+分析：1.flag用来判断是否已经有过输出～
 2.当b!=0时，因为给出的是所有非零项系数，所以必定会有输出，先判断flag是否为1，如果为1表示已经有过输出，那么在前面要先输出一个空格
 3.输出 a * b 和 b - 1，然后将flag标记为1表示已经有过输出
+4.最后判断当没有输出并且b==0的时候，输出“0 0”
 
 #include <iostream>
 using namespace std;
 int main() {
     int a, b, flag = 0;
     while (cin >> a >> b) {
-        if (flag == 0 && b == 0) cout << "0 0";
         if (b != 0) {
             if (flag == 1) cout << " ";
             cout << a * b << " " << b - 1;
             flag = 1;
-        } 
+        }
     }
+    if (flag == 0) cout << "0 0";
     return 0;
 }
