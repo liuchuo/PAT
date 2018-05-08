b24de7f96930d0871bf6aecfa24eb8d05af3afd6
liuchuo <95323362@qq.com>
2018-05-08 15:50:26 +0800

    :art: using stoi

--- PAT甲级(Advanced Level)/1132. Cut Integer (20).cpp
+++ PAT甲级(Advanced Level)/1132. Cut Integer (20).cpp
@@ -15,20 +15,16 @@ No
 No
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 int main() {
     int n, num;
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &num);
-        int len = 0, tempnum = num;
-        while (tempnum != 0) {
-            tempnum = tempnum / 10;
-            len++;
-        }
-        int d = pow(10, len / 2);
-        int a = num % d, b = num / d;
+        string s = to_string(num);
+        int len = s.length();
+        int a = stoi(s.substr(0, len/2));
+        int b = stoi(s.substr(len/2));
         if (a * b != 0 && num % (a * b) == 0)
             printf("Yes\n");
         else
