273d62e44f9d69b295b2ce2b2f2ff6b685e72948
liuchuo <95323362@qq.com>
2018-05-28 15:51:34 +0800

    :bug: Change the two-dimensional array line to map memory to solve memory overruns.

--- PAT甲级(Advanced Level)/1131. Subway Map (30) .cpp
+++ PAT甲级(Advanced Level)/1131. Subway Map (30) .cpp
@@ -40,42 +40,45 @@ Take Line#3 from 1306 to 3001.
 题目大意：找出一条路线，使得对任何给定的起点和终点，可以找出中途经停站最少的路线；如果经停站一样多，则取需要换乘线路次数最少的路线～
 分析：【很简单的，别跑^_^】一遍DFS即可～DFS过程中要维护两个变量：minCnt-中途经停的最少的站; minTransfer-需要换乘的最小次数～
 0.可以这样计算出一条线路的换乘次数：在line[10000][10000]的数组中保存每两个相邻站中间的线路是几号线～从头到尾遍历最终保存的路径，preLine为前一小段的线路编号，如果当前的结点和前一个结点组成的这条路的线路编号和preLine不同，说明有一个换乘，就将cnt+1，最后遍历完累加的cnt即是换乘的次数～
+0.1.update：由于新的PAT系统中会显示原解法有一个测试用例内存超限，考虑到line[10000][10000]存储的方式太暴力了，所以将line换成了unordered_map<int, int> line存储的方式，第一个int用来存储线路，每次将前四位存储第一个线路，后四位存储第二个线路，使用a[i-1]*10000+a[i]的方式存储，第二个int用来保存两个相邻中间的线路是几号线～
 1.可以这样计算出一条线路中途停站的次数：在dfs的时候有个变量cnt，表示当前路线是所需乘的第几个站，每次dfs时候将cnt+1表示向下遍历一层～cnt就是当前中途停站的次数～
 2.可以这样输出结果：和计算线路换乘次数的思路一样，每当preLine和当前Line值不同的时候就输出一句话～保存preTransfer表示上一个换乘站，最后不要忘记输出preTransfer和最后一个站之间的路即使最后一个站并不是换乘站～
 
-#include <cstdio>
+#include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 vector<vector<int>> v(10000);
-int line[10000][10000], visit[10000];
+int visit[10000], minCnt, minTransfer, start, end1;
+unordered_map<int, int> line;
 vector<int> path, tempPath;
 int transferCnt(vector<int> a) {
     int cnt = -1, preLine = 0;
     for (int i = 1; i < a.size(); i++) {
-        if (line[a[i-1]][a[i]] != preLine) cnt++;
-        preLine = line[a[i-1]][a[i]];
+        if (line[a[i-1]*10000+a[i]] != preLine) cnt++;
+        preLine = line[a[i-1]*10000+a[i]];
     }
     return cnt;
 }
-void dfs(int node, int end, int cnt, int &minCnt, int &minTransfer) {
-    if (node == end && (cnt < minCnt || (cnt == minCnt && transferCnt(tempPath) < minTransfer))) {
+void dfs(int node, int cnt) {
+    if (node == end1 && (cnt < minCnt || (cnt == minCnt && transferCnt(tempPath) < minTransfer))) {
         minCnt = cnt;
         minTransfer = transferCnt(tempPath);
         path = tempPath;
     }
-    if (node == end) return;
+    if (node == end1) return;
     for (int i = 0; i < v[node].size(); i++) {
         if (visit[v[node][i]] == 0) {
             visit[v[node][i]] = 1;
             tempPath.push_back(v[node][i]);
-            dfs(v[node][i], end, cnt + 1, minCnt, minTransfer);
+            dfs(v[node][i], cnt + 1);
             visit[v[node][i]] = 0;
             tempPath.pop_back();
         }
     }
 }
 int main() {
-    int n, m, k, pre, temp, a, b;
+    int n, m, k, pre, temp;
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         scanf("%d%d", &m, &pre);
@@ -83,29 +86,29 @@ int main() {
             scanf("%d", &temp);
             v[pre].push_back(temp);
             v[temp].push_back(pre);
-            line[pre][temp] = line[temp][pre] = i + 1;
+            line[pre*10000+temp] = line[temp*10000+pre] = i + 1;
             pre = temp;
         }
     }
     scanf("%d", &k);
     for (int i = 0; i < k; i++) {
-        scanf("%d%d", &a, &b);
-        int minCnt = 99999, minTransfer = 99999;
+        scanf("%d%d", &start, &end1);
+        minCnt = 99999, minTransfer = 99999;
         tempPath.clear();
-        tempPath.push_back(a);
-        visit[a] = 1;
-        dfs(a, b, 0, minCnt, minTransfer);
-        visit[a] = 0;
+        tempPath.push_back(start);
+        visit[start] = 1;
+        dfs(start, 0);
+        visit[start] = 0;
         printf("%d\n", minCnt);
-        int preLine = 0, preTransfer = a;
+        int preLine = 0, preTransfer = start;
         for (int j = 1; j < path.size(); j++) {
-            if (line[path[j-1]][path[j]] != preLine) {
+            if (line[path[j-1]*10000+path[j]] != preLine) {
                 if (preLine != 0) printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, path[j-1]);
-                preLine = line[path[j-1]][path[j]];
+                preLine = line[path[j-1]*10000+path[j]];
                 preTransfer = path[j-1];
             }
         }
-        printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, b);
+        printf("Take Line#%d from %04d to %04d.\n", preLine, preTransfer, end1);
     }
     return 0;
 }
