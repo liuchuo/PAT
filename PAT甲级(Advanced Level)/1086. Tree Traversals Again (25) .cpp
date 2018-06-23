562f7153b15f456bb10d495fb6e4b925eecdda2b
liuchuo <95323362@qq.com>
2018-06-24 04:34:16 +0800

    :bug: fix: consider the case where the values of the nodes may be the same

--- PAT甲级(Advanced Level)/1086. Tree Traversals Again (25) .cpp
+++ PAT甲级(Advanced Level)/1086. Tree Traversals Again (25) .cpp
@@ -23,18 +23,18 @@ Sample Output:
 题目大意：用栈的形式给出一棵二叉树的建立的顺序，求这棵二叉树的后序遍历
 分析：栈实现的是二叉树的中序遍历（左根右），而每次push入值的顺序是二叉树的前序遍历（根左右），所以该题可以用二叉树前序和中序转后序的方法做
 root为当前子树的根结点在前序pre中的下标，start和end为当前子树的最左边和最右边的结点在中序in中的下标。用i找到当前子树的根结点root在中序中的下标，然后左边和右边就分别为当前根结点root的左子树和右子树。递归实现~
-
+Update: Github用户littlesevenmo给我发issue提出题目并没有说所有节点的值互不相同。因此，在有多个节点的值相同的情况下，之前的代码会输出错误的结果，所以添加了key作为索引，前中后序中均保存索引值，然后用value存储具体的值，修改后的代码如下：
 
 #include <cstdio>
 #include <vector>
 #include <stack>
 #include <cstring>
 using namespace std;
-vector<int> pre, in, post;
+vector<int> pre, in, post,value;
 void postorder(int root, int start, int end) {
-    if(start > end) return ;
+    if (start > end) return;
     int i = start;
-    while(i < end && in[i] != pre[root]) i++;
+    while (i < end && in[i] != pre[root]) i++;
     postorder(root + 1, start, i - 1);
     postorder(root + 1 + i - start, i + 1, end);
     post.push_back(pre[root]);
@@ -44,23 +44,22 @@ int main() {
     scanf("%d", &n);
     char str[5];
     stack<int> s;
-    while(~scanf("%s", str)) {
-        if(strlen(str) == 4) {
+    int key=0;
+    while (~scanf("%s", str)) {
+        if (strlen(str) == 4) {
             int num;
             scanf("%d", &num);
-            pre.push_back(num);
-            s.push(num);
+            value.push_back(num);
+            pre.push_back(key);
+            s.push(key++);
         } else {
             in.push_back(s.top());
             s.pop();
         }
     }
     postorder(0, 0, n - 1);
-    printf("%d", post[0]);
-    for(int i = 1; i < n; i++)
-        printf(" %d", post[i]);
+    printf("%d", value[post[0]]);
+    for (int i = 1; i < n; i++)
+        printf(" %d",value[post[i]]);
     return 0;
 }
-
-
-
