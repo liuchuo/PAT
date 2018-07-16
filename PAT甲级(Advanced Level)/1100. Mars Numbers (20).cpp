784b9920f8dee7090ba3d3ff016ca160763333b6
liuchuo <95323362@qq.com>
2018-07-16 22:11:37 +0800

    improved func1 & func2

--- PAT甲级(Advanced Level)/1100. Mars Numbers (20).cpp
+++ PAT甲级(Advanced Level)/1100. Mars Numbers (20).cpp
@@ -24,59 +24,49 @@ may
 #include <string>
 #include <cctype>
 using namespace std;
-string a[13] = {"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct",
-    "nov", "dec"};
-string b[13] = {"", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy",
-    "lok", "mer", "jou"};
+string a[13] = { "tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct",
+"nov", "dec" };
+string b[13] = { "", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy",
+"lok", "mer", "jou" };
 void func1(string s) {
-    int len = s.length();
-    int num = 0;
-    for (int i = 0; i < len; i++) {
+    int len = s.length(), num = 0;
+    for (int i = 0; i < len; i++)
         num = num * 10 + (s[i] - '0');
-    }
-    cout << b[num / 13];
-    if((num % 13) && (num / 13)) {
-        cout << " " << a[num % 13];
-    } else if(num % 13) {
-        cout << a[num % 13];
-    } else if(num == 0) {
+    if (num / 13) {
+        cout << b[num / 13];
+        if (num % 13) cout << ' ' << a[num % 13];
+    } else {
         cout << a[num % 13];
     }
 }
-
 void func2(string s) {
-    int len = s.length();
-    int num = 0;
-    if(len == 4) {
+    int len = s.length(), num = 0;
+    if (len == 4) {
         cout << 0;
-        return ;
-    } else if (len == 3) {
+        return;
+    }
+    else if (len == 3) {
         for (int i = 1; i <= 12; i++) {
             if (s == a[i]) {
                 cout << i;
-                return ;
+                return;
             }
             if (s == b[i]) {
                 cout << i * 13;
-                return ;
+                return;
             }
         }
-    } else {
-        string temp1 = s.substr(0, 3);
-        string temp2 = s.substr(4, 3);
+    }
+    else {
+        string temp1 = s.substr(0, 3), temp2 = s.substr(4, 3);
         for (int i = 1; i <= 12; i++) {
-            if (temp1 == b[i]) {
-                num += i * 13;
-            }
-            if (temp2 == a[i]) {
-                num += i;
-            }
+            if (temp1 == b[i]) num += i * 13;
+            if (temp2 == a[i]) num += i;
         }
         cout << num;
     }
-    return ;
+    return;
 }
-
 int main() {
     int n;
     cin >> n;
@@ -84,11 +74,10 @@ int main() {
     for (int i = 0; i < n; i++) {
         string s;
         getline(cin, s);
-        if(isdigit(s[0])) {
+        if (isdigit(s[0]))
             func1(s);
-        } else {
+        else
             func2(s);
-        }
         cout << endl;
     }
     return 0;
