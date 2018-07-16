125696123a5ed37fd76a52e046842f2a2ff2626b
liuchuo <95323362@qq.com>
2018-07-16 22:12:16 +0800

    -

--- PAT甲级（Java）/1001 A+B Format (20).java
+++ PAT甲级（Java）/1001 A+B Format (20).java
@@ -1,40 +1,27 @@
-package PTA.p1001to1010;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * 1001 A+B Format (20)（20 分）
- */
-public class AplusBFormat {
-
+public class Main {
     public static void main(String[] args) throws IOException {
-        //貌似自己用流读要快一些
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] split = reader.readLine().split(" ");
         Integer x = Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
-
-
         //java 可以偷个懒 直接用decimal format
-//        DecimalFormat format = new DecimalFormat("###,###");
-//        System.out.println(format.format(x));
-
-
+        //DecimalFormat format = new DecimalFormat("###,###");
+        //System.out.println(format.format(x));
         //手动实现要比DecimalFormat效率要高一些
         char[] chars = x.toString().toCharArray();
-        int modOffset=chars.length%3;
-
+        int modOffset = chars.length % 3;
         for (int i = 0; i < chars.length; i++) {
-            if(chars[i]=='-'){
+            if(chars[i] == '-'){
                 System.out.print('-');
                 continue;
             }
             System.out.print(chars[i]-48);
-            if(i==chars.length-1){
+            if(i == chars.length - 1){
                 System.out.println();
-            }
-            else if((i+1)%3==modOffset){
+            } else if((i+1) % 3 == modOffset){
                 System.out.print(',');
             }
         }
