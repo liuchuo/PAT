69db79c505fbee9470446197c69a8f98386cb62b
黄忠雨 <hdvsyu@foxmail.com>
2018-07-28 16:00:44 +0800

    refactor: new project construct for java

--- Advanced-Java/src/main/java/solution_1001/Main.java
+++ Advanced-Java/src/main/java/solution_1001/Main.java
@@ -1,3 +1,5 @@
+package solution_1001;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,21 +9,21 @@ public class Main {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] split = reader.readLine().split(" ");
         Integer x = Integer.valueOf(split[0]) + Integer.valueOf(split[1]);
-        //java 可以偷个懒 直接用decimal format
-        //DecimalFormat format = new DecimalFormat("###,###");
-        //System.out.println(format.format(x));
-        //手动实现要比DecimalFormat效率要高一些
+        // Java 可以偷个懒 直接用decimal format
+        // DecimalFormat format = new DecimalFormat("###, ###");
+        // System.out.println(format.format(x));
+        // 手动实现要比DecimalFormat效率要高一些
         char[] chars = x.toString().toCharArray();
         int modOffset = chars.length % 3;
         for (int i = 0; i < chars.length; i++) {
-            if(chars[i] == '-'){
+            if (chars[i] == '-') {
                 System.out.print('-');
                 continue;
             }
-            System.out.print(chars[i]-48);
-            if(i == chars.length - 1){
+            System.out.print(chars[i] - 48);
+            if (i == chars.length - 1) {
                 System.out.println();
-            } else if((i+1) % 3 == modOffset){
+            } else if ((i + 1) % 3 == modOffset) {
                 System.out.print(',');
             }
         }
