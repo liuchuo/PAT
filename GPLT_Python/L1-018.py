0d8af33880334bfb840f4f2fcf854a551d393c5f
Zihao Zhu <38643169+zhuzihao-hz@users.noreply.github.com>
2018-08-19 16:59:05 +0800

    Update L1-018.py

--- GPLT_Python/L1-018.py
+++ GPLT_Python/L1-018.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul 15 16:54:40 2018
-
-@author: zhuzihao
-"""
-
 N=input().split(':')
 if int(N[0])<13:
     print('Only '+N[0]+':'+N[1]+'.  Too early to Dang.')
