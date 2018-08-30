59d47f1a29cc2fc6c0e1a36eb7f5991a20073b86
liuchuo <95323362@qq.com>
2018-08-30 14:00:32 +0800

    :bug: bug: fix a little bug

--- BasicLevel_C++/1032. 挖掘机技术哪家强(20).cpp
+++ BasicLevel_C++/1032. 挖掘机技术哪家强(20).cpp
@@ -23,14 +23,14 @@ using namespace std;
 int main() {
     int N;
     cin >> N;
-    vector<int> a(N);
+    vector<int> a(N + 1);
     int num, score;
     for (int i = 0; i < N; i++) {
         cin >> num >> score;
         a[num] += score;
     }
     int max = a[1], t = 1;
-    for (int i = 1; i < N; i++) {
+    for (int i = 2; i <= N; i++) {
         if (max < a[i]) {
             max = a[i];
             t = i;
