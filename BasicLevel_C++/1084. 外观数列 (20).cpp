95841bcd78b61bcbada37f74d76b800cfaede5c0
liuchuo <95323362@qq.com>
2018-12-04 00:29:52 +0800

    :zap: perf: improve performance

--- BasicLevel_C++/1084. 外观数列 (20).cpp
+++ BasicLevel_C++/1084. 外观数列 (20).cpp
@@ -8,7 +8,7 @@ int main() {
         string t;
         for (int i = 0; i < s.length(); i = j) {
             for (j = i; j < s.length() && s[j] == s[i]; j++);
-            t += to_string((s[i] - '0') * 10 + j - i);
+            t += s[i] + to_string(j - i);
         }
         s = t;
     }
