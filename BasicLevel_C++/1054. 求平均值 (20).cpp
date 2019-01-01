f490cb1aa5d3dceaca06d59cb84a999898d6ac10
liuchuo <95323362@qq.com>
2019-01-01 23:09:42 +0800

    Update 1054. 求平均值 (20).cpp

--- BasicLevel_C++/1054. 求平均值 (20).cpp
+++ BasicLevel_C++/1054. 求平均值 (20).cpp
@@ -10,11 +10,10 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%s", a);
         sscanf(a, "%lf", &temp);
-        sprintf(b, "%.2lf",temp);
+        sprintf(b, "%.2f",temp);
         int flag = 0;
-        for(int j = 0; j < strlen(a); j++) {
+        for(int j = 0; j < strlen(a); j++)
             if(a[j] != b[j]) flag = 1;
-        }
         if(flag || temp < -1000 || temp > 1000) {
             printf("ERROR: %s is not a legal number\n", a);
             continue;
@@ -23,12 +22,11 @@ int main() {
             cnt++;
         }
     }
-    if(cnt == 1) {
-        printf("The average of 1 number is %.2lf", sum);
-    } else if(cnt > 1) {
-        printf("The average of %d numbers is %.2lf", cnt, sum / cnt);
-    } else {
+    if(cnt == 1)
+        printf("The average of 1 number is %.2f", sum);
+    else if(cnt > 1)
+        printf("The average of %d numbers is %.2f", cnt, sum / cnt);
+    else
         printf("The average of 0 numbers is Undefined");
-    }
     return 0;
 }
