ee71b780c1524ea8514b1559d5e4beddb16a0bab
liuchuo <95323362@qq.com>
2019-01-08 21:37:24 +0800

    :tada: feat: initial commit for basic 1044

--- BasicLevel_C++/1044. 火星数字(20).cpp
+++ BasicLevel_C++/1044. 火星数字(20).cpp
@@ -1,71 +1,37 @@
 #include <iostream>
 #include <string>
 using namespace std;
-void func1(string s);
-void func2(string s);
+string a[13] = {"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
+string b[13] = {"####", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
+string s;
+int len;
+void func1(int t) {
+    if (t / 13) cout << b[t / 13];
+    if ((t / 13) && (t % 13)) cout << " ";
+    if (t % 13 || t == 0) cout << a[t % 13];
+}
+void func2() {
+    int t1 = 0, t2 = 0;
+    string s1 = s.substr(0, 3), s2;
+    if (len > 4) s2 = s.substr(4, 3);
+    for (int j = 1; j <= 12; j++) {
+        if (s1 == a[j] || s2 == a[j]) t2 = j;
+        if (s1 == b[j]) t1 = j;
+    }
+    cout << t1 * 13 + t2;
+}
 int main() {
     int n;
     cin >> n;
     getchar();
     for (int i = 0; i < n; i++) {
-        string s;
         getline(cin, s);
-        int leng = s.length();
-        if (leng == 1 && s[0] == '0') {
-            cout << "tret" << endl;
-        } else {
-            if (s[0] >= '0' && s[0] <= '9') {
-                func1(s);
-            } else {
-                func2(s);
-            }
-        }
+        len = s.length();
+        if (s[0] >= '0' && s[0] <= '9')
+            func1(stoi(s));
+        else
+            func2();
+        cout << endl;
     }
     return 0;
-}
-void func1(string s) {
-    string a[13] = {"###", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-    string b[13] = {"###", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
-    int len = s.length();
-    int t;
-    if (len == 1) {
-        t = s[0] - '0';
-    } else if(len == 2) {
-        t = (s[0] - '0') * 10 + (s[1] - '0');
-    } else {
-        t = (s[0] - '0') * 100 + (s[1] - '0') * 10 + (s[2] - '0');
-    }
-    if (t <= 12) {
-        cout << a[t] << endl;
-    } else if (t % 13 == 0) {
-        cout << b[t / 13] << endl;
-    } else {
-        cout << b[t / 13] << " " << a[t % 13] << endl;
-    }
-}
-void func2(string s) {
-    string a[13] = {"###", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
-    string b[13] = {"###", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
-    int len = s.length();
-    if (len == 3) {
-        for (int j = 1; j <= 12; j++) {
-            if (s[0] == a[j][0] && s[1] == a[j][1] && s[2] == a[j][2]) {
-                cout << j << endl;
-            } else if (s[0] == b[j][0] && s[1] == b[j][1] && s[2] == b[j][2]) {
-                cout << 13 * j << endl;
-            }
-        }
-    } else {
-        int t1 = 0;
-        int t2 = 0;
-        for (int j = 1; j <= 12; j++) {
-            if (s[0] == b[j][0] && s[1] == b[j][1] && s[2] == b[j][2]) {
-                t1 = j;
-            }
-            if (s[4] == a[j][0] && s[5] == a[j][1] && s[6] == a[j][2]) {
-                t2 = j;
-            }
-        }
-        cout << t1 * 13 + t2 << endl;
-    }
 }
