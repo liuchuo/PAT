edc49ac922ed9f6669f50dc442e134ffc6dcfe80
liuchuo <95323362@qq.com>
2019-02-22 00:24:19 +0800

    :bug: bug: fix a little bug~

--- AdvancedLevel_C++/1005. Spell It Right (20).cpp
+++ AdvancedLevel_C++/1005. Spell It Right (20).cpp
@@ -10,6 +10,6 @@ int main() {
     string arr[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     cout << arr[s[0] - '0'];
     for (int i = 1; i < s.length(); i++)
-        cout << " " << arr[s[1] - '0'];
+        cout << " " << arr[s[i] - '0'];
     return 0;
 }
