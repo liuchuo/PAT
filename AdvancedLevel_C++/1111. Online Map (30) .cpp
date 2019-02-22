63b68e41cfcf99cd503f3780c34d1f38042080a5
SuYindu <suyindu@zju.edu.cn>
2019-02-23 01:53:17 +0800

    Update AdvancedLevel_C++/1111. Online Map (30) .cpp

--- AdvancedLevel_C++/1111. Online Map (30) .cpp
+++ AdvancedLevel_C++/1111. Online Map (30) .cpp
@@ -1,108 +1,91 @@
-#include <iostream>
-#include <algorithm>
+#include <cstdio>
 #include <vector>
+#include <queue>
+#include <deque>
 using namespace std;
-const int inf = 999999999;
-int dis[510], Time[510], e[510][510], w[510][510], dispre[510],Timepre[510], weight[510],NodeNum[510];
-bool visit[510];
-vector<int> dispath, Timepath, temppath;
-int st, fin, minnode = inf;
-void dfsdispath(int v) {
-    dispath.push_back(v);
-    if(v == st) return ;
-    dfsdispath(dispre[v]);
+
+int INFINITY = 0x3f3f3f3f;
+
+vector<int> Dijkstra(int src, vector<vector<int>> &graph1, vector<vector<int>> &graph2) {
+    int n = graph1.size();
+    vector<int> prev(n, -1);
+    vector<int> dist1(n, INFINITY);
+    vector<int> dist2(n, INFINITY);
+    dist1[src] = dist2[src] = 0;
+    queue<int> q;
+    q.push(src);
+    while (!q.empty()) {
+        int u = q.front(); q.pop();
+        for (int v = 0; v < n; v++) {
+            if ((dist1[u] + graph1[u][v] < dist1[v]) 
+            || (dist1[u] + graph1[u][v] == dist1[v] && dist2[u] + graph2[u][v] < dist2[v])) {
+                dist1[v] = dist1[u] + graph1[u][v];
+                dist2[v] = dist2[u] + graph2[u][v];
+                prev[v] = u;
+                q.push(v);
+            }
+        }
+    }
+    return prev;
 }
-void dfsTimepath(int v) {
-    Timepath.push_back(v);
-    if(v == st) return ;
-    dfsTimepath(Timepre[v]);
+
+int ConstructPath(int dest, vector<vector<int>> &graph, vector<int> &prev, deque<int> &path) {
+    int result = 0;
+    int cur = prev[dest], next = dest;
+    while (cur != -1) {
+        result += graph[cur][next];
+        path.push_front(next);
+        next = cur;
+        cur = prev[cur];
+    }
+    path.push_front(next);
+    return result;
 }
+
+int ShortestPath(int src, int dest, deque<int> &path, 
+vector<vector<int>> &graph1, vector<vector<int>> &graph2) {
+    vector<int> prev = Dijkstra(src, graph1, graph2);
+    return ConstructPath(dest, graph1, prev, path);
+}
+
+bool Equal(deque<int> &p1, deque<int> &p2) {
+    if (p1.size() != p2.size()) return false;
+    for (int i = 0; i < p1.size(); i++)
+        if (p1[i] != p2[i]) return false;
+    return true;
+}
+
+void Print(deque<int> &path) {
+    for (int i = 0; i < path.size(); i++) {
+        if (i != 0) printf(" -> ");
+        printf("%d", path[i]);
+    }
+    printf("\n");
+}
+
 int main() {
-    fill(dis, dis + 510, inf);
-    fill(Time, Time + 510, inf);
-    fill(weight, weight + 510, inf);
-    fill(e[0], e[0] + 510 * 510, inf);
-    fill(w[0], w[0] + 510 * 510, inf);
     int n, m;
-    scanf("%d %d", &n, &m);
-    int a, b, flag, len, t;
-    for(int i = 0; i < m; i++) {
-        scanf("%d %d %d %d %d", &a, &b, &flag, &len, &t);
-        e[a][b] = len;
-        w[a][b] = t;
-        if(flag != 1) {
-            e[b][a] = len;
-            w[b][a] = t;
-        }
+    scanf("%d%d", &n, &m);
+    vector<vector<int>> temp_graph(n, vector<int>(n, 1)); // 无权图
+    vector<vector<int>> dist_graph(n, vector<int>(n, INFINITY));
+    vector<vector<int>> time_graph(n, vector<int>(n, INFINITY));
+    while (m--) {
+        int u, v, flag, dist, time;
+        scanf("%d%d%d%d%d", &u, &v, &flag, &dist, &time);
+        dist_graph[u][v] = dist; time_graph[u][v] = time;
+        if (flag != 1) { dist_graph[v][u] = dist; time_graph[v][u] = time; }
     }
-    scanf("%d %d", &st, &fin);
-    dis[st] = 0;
-    for(int i = 0; i < n; i++)
-        dispre[i] = i;
-    for(int i = 0; i < n; i++) {
-        int u = -1, minn = inf;
-        for(int j = 0; j < n; j++) {
-            if(visit[j] == false && dis[j] < minn) {
-                u = j;
-                minn = dis[j];
-            }
-        }
-        if(u == -1) break;
-        visit[u] = true;
-        for(int v = 0; v < n; v++) {
-            if(visit[v] == false && e[u][v] != inf) {
-                if(e[u][v] + dis[u] < dis[v]) {
-                    dis[v] = e[u][v] + dis[u];
-                    dispre[v] = u;
-                    weight[v] = weight[u] + w[u][v];
-                } else if(e[u][v] + dis[u] == dis[v] && weight[v] > weight[u] + w[u][v]) {
-                    weight[v] = weight[u] + w[u][v];
-                    dispre[v] = u;
-                }
-            }
-        }
-    }
-    dfsdispath(fin);
-    Time[st] = 0;
-    fill(visit, visit + 510, false);
-    for(int i = 0; i < n; i++) {
-        int u = -1, minn = inf;
-        for(int j = 0; j < n; j++) {
-            if(visit[j] == false && minn > Time[j]) {
-                u = j;
-                minn = Time[j];
-            }
-        }
-        if(u == -1) break;
-        visit[u] = true;
-        for(int v = 0; v < n; v++) {
-            if(visit[v] == false && w[u][v] != inf) {
-                if(w[u][v] + Time[u] < Time[v]) {
-                    Time[v] = w[u][v] + Time[u];
-                    Timepre[v]=(u);
-                    NodeNum[v]=NodeNum[u]+1;
-                } else if(w[u][v] + Time[u] == Time[v]&&NodeNum[u]+1<NodeNum[v]) {
-                    Timepre[v]=(u);
-                    NodeNum[v]=NodeNum[u]+1;
-                }
-            }
-        }
-    }
-    dfsTimepath(fin);
-    printf("Distance = %d", dis[fin]);
-    if(dispath == Timepath) {
-        printf("; Time = %d: ", Time[fin]);
+    int src, dest;
+    scanf("%d%d", &src, &dest);
+    deque<int> shortest_path, quickest_path;
+    int min_dist = ShortestPath(src, dest, shortest_path, dist_graph, time_graph);
+    int min_time = ShortestPath(src, dest, quickest_path, time_graph, temp_graph);
+    if (!Equal(shortest_path, quickest_path)) {
+        printf("Distance = %d: ", min_dist); Print(shortest_path);
+        printf("Time = %d: "    , min_time); Print(quickest_path);
     } else {
-        printf(": ");
-        for(int i = dispath.size() - 1; i >= 0; i--) {
-            printf("%d", dispath[i]);
-            if(i != 0) printf(" -> ");
-        }
-        printf("\nTime = %d: ", Time[fin]);
-    }
-    for(int i = Timepath.size() - 1; i >= 0; i--) {
-        printf("%d", Timepath[i]);
-        if(i != 0) printf(" -> ");
+        printf("Distance = %d; Time = %d: ", min_dist, min_time);
+        Print(shortest_path);
     }
     return 0;
 }
