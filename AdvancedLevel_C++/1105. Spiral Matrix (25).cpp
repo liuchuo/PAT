70fd91a052328e20eeefae27ccf9e8d718a8ab39
SuYindu <suyindu@zju.edu.cn>
2019-02-23 22:05:44 +0800

    Update AdvancedLevel_C++/1105. Spiral Matrix (25).cpp

--- AdvancedLevel_C++/1105. Spiral Matrix (25).cpp
+++ AdvancedLevel_C++/1105. Spiral Matrix (25).cpp
@@ -1,45 +1,35 @@
-#include <iostream>
 #include <cmath>
-#include <vector>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-int func(int N) {
-    int i = sqrt((double)N);
-    while(i >= 1) {
-        if(N % i == 0)
-            return i;
-        i--;
-    }
-    return 1;
-}
-int cmp(int a, int b) {return a > b;}
+
 int main() {
-    int N, m, n, t = 0;
-    scanf("%d", &N);
-    n = func(N);
-    m = N / n;
-    vector<int> a(N);
-    for (int i = 0; i < N; i++)
-        scanf("%d", &a[i]);
-    sort(a.begin(), a.end(), cmp);
-    vector<vector<int> > b(m, vector<int>(n));
-    int level = m / 2 + m % 2;
-    for (int i = 0; i < level; i++) {
-        for (int j = i; j <= n - 1 - i && t <= N - 1; j++)
-                b[i][j] = a[t++];
-        for (int j = i + 1; j <= m - 2 - i && t <= N - 1; j++)
-                b[j][n - 1 - i] = a[t++];
-        for (int j = n - i - 1; j >= i && t <= N - 1; j--)
-                b[m - 1 - i][j] = a[t++];
-        for (int j = m - 2 - i; j >= i + 1 && t <= N - 1; j--)
-                b[j][i] = a[t++];
+    int n, m, k;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++) cin >> nums[i];
+    sort(nums.begin(), nums.end(), greater<int>());
+    for (k = sqrt(n); n % k != 0; k--);
+    m = n / k;
+    vector<vector<int>> ans(m, vector<int>(k, 0));
+    int i = 0, row = 0, col = 0;
+    while (i < n) {
+        // 向右填充
+        while (i < n && col <  k && ans[row][col] == 0) ans[row][col++] = nums[i++];
+        row++; col--;
+        // 向下填充
+        while (i < n && row <  m && ans[row][col] == 0) ans[row++][col] = nums[i++];
+        row--; col--;
+        // 向左填充
+        while (i < n && col >= 0 && ans[row][col] == 0) ans[row][col--] = nums[i++];
+        row--; col++;
+        // 向上填充
+        while (i < n && row >= 0 && ans[row][col] == 0) ans[row--][col] = nums[i++];
+        row++; col++;
     }
-    for (int i = 0; i < m; i++) {
-        for (int j = 0 ; j < n; j++) {
-            printf("%d", b[i][j]);
-            if (j != n - 1) printf(" ");
-        }
-        printf("\n");
-    }
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < k; j++)
+            cout << ans[i][j] << (j < k - 1 ? ' ' : '\n');
     return 0;
 }
