b062876466ed018eb319b30231376c5c38836f81
SuYindu <suyindu@zju.edu.cn>
2019-02-23 21:08:39 +0800

    Update AdvancedLevel_C++/1104. Sum of Number Segments (20).cpp

--- AdvancedLevel_C++/1104. Sum of Number Segments (20).cpp
+++ AdvancedLevel_C++/1104. Sum of Number Segments (20).cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
+
 int main() {
     int n;
     cin >> n;
-    double a[100001];
-    double sum = 0.0;
+    double num, sum = 0.0;
     for (int i = 1; i <= n; i++) {
-        cin >> a[i];
-        sum = sum + a[i] * i * (n - i + 1);
+        cin >> num;
+        sum += num * i * (n - i + 1);
     }
-    printf("%.2f", sum);
+    printf("%.2f\n", sum);
     return 0;
 }
