dd1a8a67ed70e8a35e7f0e271c22f4dc123e3c66
liuchuo <95323362@qq.com>
2019-02-28 04:55:08 +0800

    :art: improve format of the code

--- CCCC-GPLT/L1-006. 连续因子.cpp
+++ CCCC-GPLT/L1-006. 连续因子.cpp
@@ -4,22 +4,24 @@ using namespace std;
 long int num, temp;
 int main(){
     cin >> num;
-    int first = 0, len = 0, maxn = sqrt(num);
-    for (int i = 2; i <= maxn; i++){
-        int j; temp = 1;
-        for (j = i; j <= maxn; j++){
+    int first = 0, len = 0, maxn = sqrt(num) + 1;
+    for (int i = 2; i <= maxn; i++) {
+        int j; 
+        temp = 1;
+        for (j = i; j <= maxn; j++) {
             temp *= j;
             if (num % temp != 0) break;
         }
-        if (j - i > len){
+        if (j - i > len) {
             len = j - i;
             first = i;
         }
     }
-    if (first == 0) cout << 1 << endl << num;
-    else {
+    if (first == 0) {
+        cout << 1 << endl << num;
+    } else {
         cout << len << endl;
-        for (int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             cout << first + i;
             if (i != len - 1) cout << '*';
         }
