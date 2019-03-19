8f2b0369e2b1e78e58171dbf5f4b3a28acf9df87
liuchuo <95323362@qq.com>
2019-03-20 02:28:14 +0800

    :hammer: refactor code

--- BasicLevel_C++/1037. 在霍格沃茨找零钱（20）.cpp
+++ BasicLevel_C++/1037. 在霍格沃茨找零钱（20）.cpp
@@ -1,47 +1,16 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int a, b ,c;
-    int m, n, t;
-    scanf("%d.%d.%d",&a, &b, &c);
-    scanf("%d.%d.%d",&m, &n, &t);
-    int flag;
-    if (a > m ||(a == m && b > n) ||(a == m && b == n && c > t))
-        flag = 0;
-    else
-        flag = 1;
-    
-    int x, y, z;
-    if (flag == 1) {
-        if (t >= c) {
-            z = t - c;
-        } else {
-            z = t + 29 - c;
-            n = n - 1;
-        }
-        if (n >= b) {
-            y = n - b;
-        } else {
-            y = n + 17 - b;
-            m = m - 1;
-        }
-        x = m - a;
-        cout << x << "." << y << "." << z;
-    } else {
-        if (c >= t) {
-            z = c - t;
-        } else {
-            z = c + 29 - t;
-            b = b - 1;
-        }
-        if (b >= n) {
-            y = b - n;
-        } else {
-            y = b + 17 - n;
-            a = a - 1;
-        }
-        x = a - m;
-        cout << "-" << x << "." << y << "." << z;
+    int a, b ,c, m, n, t, x, y, z;
+    scanf("%d.%d.%d %d.%d.%d",&a, &b, &c, &m, &n, &t);
+    if (a > m || (a == m && b > n) || (a == m && b == n && c > t)) {
+        swap(a, m); swap(b, n); swap(c, t);
+        printf("-");        
     }
+    z = t < c ? t - c + 29 : t - c;
+    n = t < c ? n - 1 : n;
+    y = n < b ? n - b + 17 : n - b;
+    x = n < b ? m - a - 1 : m - a;
+    printf("%d.%d.%d", x, y, z);
     return 0;
 }
