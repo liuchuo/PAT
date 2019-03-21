6931e9353e06044f2014359917c9ed7f80f7b4d2
liuchuo <95323362@qq.com>
2019-03-22 07:05:14 +0800

    :hammer: refactor

--- BasicLevel_C++/1031. 查验身份证(15).cpp
+++ BasicLevel_C++/1031. 查验身份证(15).cpp
@@ -1,38 +1,27 @@
 #include <iostream>
-#include <string>
 using namespace std;
-bool func(string a);
+int a[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
+int b[11] = {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2};
+string s;
+bool isTrue() {
+    int sum = 0;
+    for (int i = 0; i < 17; i++) {
+        if (s[i] < '0' || s[i] > '9') return false;
+        sum += (s[i] - '0') * a[i];
+    }
+    int temp = (s[17] == 'X') ? 10 : (s[17] - '0');
+    return b[sum%11] == temp;
+}
 int main() {
-    int n, count = 0;
+    int n, flag = 0;
     cin >> n;
     for (int i = 0; i < n; i++) {
-        string a;
-        cin >> a;
-        if (func(a)) count++;
-    }
-    if (count == 0) cout << "All passed";
-    return 0;
-}
-bool func(string s) {
-    int sum = 0, a[18];
-    for (int i = 0; i < 17; i++) {
-        if (s[i] >= '0' && s[i] <= '9') {
-            a[i] = s[i] - '0';
-        } else {
+        cin >> s;
+        if (!isTrue()) {
             cout << s << endl;
-            return true;
-            break;
+            flag = 1;
         }
     }
-    a[17] = (s[17] == 'X') ? 10 : (s[17] - '0');
-    int b[17] = {7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2};
-    for (int i = 0; i < 17; i++)
-        sum = sum + a[i] * b[i];
-    sum = sum % 11;
-    int c[11] = {1, 0, 10, 9, 8, 7, 6, 5, 4, 3, 2};
-    if (c[sum] != a[17]) {
-        cout << s << endl;
-        return true;
-    }
-    return false;
+    if (flag == 0) cout << "All passed";
+    return 0;
 }
