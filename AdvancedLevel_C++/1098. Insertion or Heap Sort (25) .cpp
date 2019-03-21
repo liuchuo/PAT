be56aa1895a9a1e9ff9e8157a241a7540f6a71ba
liuchuo <95323362@qq.com>
2019-03-22 07:06:00 +0800

    :zap: perf: improve performance

--- AdvancedLevel_C++/1098. Insertion or Heap Sort (25) .cpp
+++ AdvancedLevel_C++/1098. Insertion or Heap Sort (25) .cpp
@@ -1,47 +1,37 @@
-#include <cstdio>
-#include <vector>
+#include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
-vector<int> a, b;
-void downAdjust(int low, int high) {
+void downAdjust(vector<int> &b, int low, int high) {
     int i = 1, j = i * 2;
     while(j <= high) {
-        if(j + 1 <= high && b[j] < b[j + 1])
-            j = j + 1;
-        if(b[i] < b[j]) {
-            swap(b[i], b[j]);
-            i = j; j = i * 2;
-        } else {
-            break;
-        }
+        if(j + 1 <= high && b[j] < b[j + 1]) j = j + 1;
+        if (b[i] >= b[j]) break;
+        swap(b[i], b[j]);
+        i = j; j = i * 2;
     }
 }
 int main() {
     int n, p = 2;
     scanf("%d", &n);
-    a.resize(n + 1), b.resize(n + 1);
-    for(int i = 1; i <= n; i++)
-        scanf("%d", &a[i]);
-    for(int i = 1; i <= n; i++)
-        scanf("%d", &b[i]);
+    vector<int> a(n + 1), b(n + 1);
+    for(int i = 1; i <= n; i++) scanf("%d", &a[i]);
+    for(int i = 1; i <= n; i++) scanf("%d", &b[i]);
     while(p <= n && b[p - 1] <= b[p]) p++;
     int index = p;
     while(p <= n && a[p] == b[p]) p++;
     if(p == n + 1) {
         printf("Insertion Sort\n");
         sort(b.begin() + 1, b.begin() + index + 1);
-        printf("%d", b[1]);
-        for(int i = 2; i <= n; i++)
-            printf(" %d", b[i]);
     } else {
         printf("Heap Sort\n");
         p = n;
-        while(p >= 2 && b[p] >= b[p - 1]) p--;
+        while(p > 2 && b[p] >= b[1]) p--;
         swap(b[1], b[p]);
-        downAdjust(1, p - 1);
-        printf("%d", b[1]);
-        for(int i = 2; i <= n; i++)
-            printf(" %d", b[i]);
+        downAdjust(b, 1, p - 1);
     }
+    printf("%d", b[1]);
+    for(int i = 2; i <= n; i++)
+        printf(" %d", b[i]);
     return 0;
 }
