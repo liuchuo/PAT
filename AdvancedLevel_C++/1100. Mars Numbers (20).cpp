0ea0af20c9bf40f02fe086c3ce988c2389e91387
liuchuo <95323362@qq.com>
2019-04-18 00:05:43 +0800

    :art: style: improve structure of the code

--- AdvancedLevel_C++/1100. Mars Numbers (20).cpp
+++ AdvancedLevel_C++/1100. Mars Numbers (20).cpp
@@ -1,58 +1,36 @@
 #include <iostream>
 #include <string>
-#include <cctype>
 using namespace std;
-string a[13] = { "tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec" };
-string b[13] = { "", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou" };
-void func1(string s) {
-    int len = s.length(), num = 0;
-    for (int i = 0; i < len; i++)
-        num = num * 10 + (s[i] - '0');
-    if (num / 13) {
-        cout << b[num / 13];
-        if (num % 13) cout << ' ' << a[num % 13];
-    } else {
-        cout << a[num % 13];
-    }
+string a[13] = {"tret", "jan", "feb", "mar", "apr", "may", "jun", "jly", "aug", "sep", "oct", "nov", "dec"};
+string b[13] = {"####", "tam", "hel", "maa", "huh", "tou", "kes", "hei", "elo", "syy", "lok", "mer", "jou"};
+string s;
+int len;
+void func1(int t) {
+    if (t / 13) cout << b[t / 13];
+    if ((t / 13) && (t % 13)) cout << " ";
+    if (t % 13 || t == 0) cout << a[t % 13];
 }
-void func2(string s) {
-    int len = s.length(), num = 0;
-    if (len == 4) {
-        cout << 0;
-        return;
-    } else if (len == 3) {
-        for (int i = 1; i <= 12; i++) {
-            if (s == a[i]) {
-                cout << i;
-                return;
-            }
-            if (s == b[i]) {
-                cout << i * 13;
-                return;
-            }
-        }
+void func2() {
+    int t1 = 0, t2 = 0;
+    string s1 = s.substr(0, 3), s2;
+    if (len > 4) s2 = s.substr(4, 3);
+    for (int j = 1; j <= 12; j++) {
+        if (s1 == a[j] || s2 == a[j]) t2 = j;
+        if (s1 == b[j]) t1 = j;
     }
-    else {
-        string temp1 = s.substr(0, 3), temp2 = s.substr(4, 3);
-        for (int i = 1; i <= 12; i++) {
-            if (temp1 == b[i]) num += i * 13;
-            if (temp2 == a[i]) num += i;
-        }
-        cout << num;
-    }
-    return;
+    cout << t1 * 13 + t2;
 }
 int main() {
     int n;
     cin >> n;
     getchar();
     for (int i = 0; i < n; i++) {
-        string s;
         getline(cin, s);
-        if (isdigit(s[0]))
-            func1(s);
+        len = s.length();
+        if (s[0] >= '0' && s[0] <= '9')
+            func1(stoi(s));
         else
-            func2(s);
+            func2();
         cout << endl;
     }
     return 0;
