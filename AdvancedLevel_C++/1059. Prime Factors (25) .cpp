148784164eb898cb6df4bd691564939424f30ddb
liuchuo <95323362@qq.com>
2019-09-01 22:11:53 +0800

    :bug: bug: change 500000 into 50000

--- AdvancedLevel_C++/1059. Prime Factors (25) .cpp
+++ AdvancedLevel_C++/1059. Prime Factors (25) .cpp
@@ -1,17 +1,17 @@
 #include <cstdio>
 #include <vector>
 using namespace std;
-vector<int> prime(500000, 1);
+vector<int> prime(50000, 1);
 int main() {
-    for(int i = 2; i * i < 500000; i++)
-        for(int j = 2; j * i < 500000; j++)
+    for(int i = 2; i * i < 50000; i++)
+        for(int j = 2; j * i < 50000; j++)
             prime[j * i] = 0;
     long int a;
     scanf("%ld", &a);
     printf("%ld=", a);
     if(a == 1) printf("1");
     bool state = false;
-    for(int i = 2; a >= 2;i++) {
+    for(int i = 2; i < 50000 && a >= 2; i++) {
         int cnt = 0, flag = 0;
         while(prime[i] == 1 && a % i == 0) {
             cnt++;
@@ -23,8 +23,8 @@ int main() {
             printf("%d", i);
             state = true;
         }
-        if(cnt >= 2)
-            printf("^%d", cnt);
+        if(cnt >= 2) printf("^%d", cnt);
     }
+    if (a > 1) printf("%s%ld", state ? "*" : "", a);
     return 0;
 }
