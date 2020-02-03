e565ed0a825a4e06e9064a7ceac7bc9b0b3ea8a0
hdvsyu <hdvsyu@foxmail.com>
2020-02-03 17:15:04 +0800

    fixed #128

--- AdvancedLevel_C++/1064. Complete Binary Search Tree (30).cpp
+++ AdvancedLevel_C++/1064. Complete Binary Search Tree (30).cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 using namespace std;
-vector<int> in, level;
-void levelorder(int start, int end, int index) {
-    if(start > end) return ;
-    int n = end - start + 1;
-    int l = log(n + 1) / log(2); // 除了最后一层的层数
-    int leave = n - (pow(2, l) - 1);// 最后一层的叶子节点数
-    int root = start + (pow(2, l - 1) - 1) + min((int)pow(2, l - 1), leave); // pow(2, l - 1) - 1是除了root结点所在层和最后一层外，左子树的结点个数，pow(2, l - 1) 是l+1层最多拥有的属于根结点左子树的结点个数，min(pow(2, l - 1), leave)是最后一个结点真正拥有的属于根结点左子树上的结点个数
-    level[index] = in[root];
-    levelorder(start, root - 1, 2 * index + 1);
-    levelorder(root + 1, end, 2 * index + 2);
+int n, cnt = 0;
+// cbt is Complete Binary Search Tree
+vector<int> in, cbt;
+void inOrder(int index) {
+    if (index >= n) return;
+    inOrder(2 * index + 1);
+    cbt[index] = in[cnt++];
+    inOrder(2 * index + 2);
 }
 int main() {
-    int n;
-    scanf("%d", &n);
+    cin >> n;
     in.resize(n);
-    level.resize(n);
-    for(int i = 0 ; i < n; i++)
-        scanf("%d", &in[i]);
+    cbt.resize(n);
+    for (int i = 0; i < n; i++) scanf("%d", &in[i]);
     sort(in.begin(), in.end());
-    levelorder(0, n - 1, 0);
-    printf("%d", level[0]);
-    for(int i = 1; i < n; i++)
-        printf(" %d", level[i]);
+    inOrder(0);
+    for (int i = 0; i < n; i++) {
+        if (i != 0) printf(" ");
+        printf("%d", cbt[i]);
+    }
     return 0;
 }
