6cf26047ffdb0e1c1914ee9fbd5d64f610fcc17e
碗碗 <43742453+Surbowl@users.noreply.github.com>
2020-02-20 09:16:43 +0800

    Update 1001. 害死人不偿命的(3n+1)猜想.cpp
    使用位操作判断奇偶性，性能更好。

--- BasicLevel_C++/1001. 害死人不偿命的(3n+1)猜想 (15).cpp
+++ BasicLevel_C++/1001. 害死人不偿命的(3n+1)猜想 (15).cpp
@@ -4,7 +4,7 @@ int main() {
     int n, count = 0;
     cin >> n;
     while (n != 1) {
-        if (n % 2 != 0) n = 3 * n + 1;
+        if (n & 1) n = 3 * n + 1;
         n = n / 2;
         count++;
     }
