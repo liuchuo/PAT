92142b88153cd92e7a6e7e31ff911bb71508e521
liuchuo <95323362@qq.com>
2020-04-07 15:39:52 +0800

    Update 1058. A+B in Hogwarts (20).cpp

--- AdvancedLevel_C++/1058. A+B in Hogwarts (20).cpp
+++ AdvancedLevel_C++/1058. A+B in Hogwarts (20).cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    long long a, b, c, d, e, f;
-    scanf("%lld.%lld.%lld %lld.%lld.%lld", &a, &b, &c, &d, &e, &f);
-    long long num = c + b * 29 + a * 29 * 17 + f + e * 29 + d * 29 * 17;
-    long long g = num / (17 * 29);
-    num = num % (17 * 29);
-    printf("%lld.%lld.%lld", g, num / 29, num % 29);
+    int a1, b1, c1, a2, b2, c2, a, b, c;
+    scanf("%d.%d.%d %d.%d.%d", &a1, &b1, &c1, &a2, &b2, &c2);
+    c = c1 + c2;
+    b = b1 + b2 + c / 29; c = c % 29;
+    a = a1 + a2 + b / 17; b = b % 17;
+    printf("%d.%d.%d\n", a, b, c);
     return 0;
 }
