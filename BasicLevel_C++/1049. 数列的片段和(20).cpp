44ffa99515cf14dd5a61be4946b4b82d4b3dc3fa
liuchuo <95323362@qq.com>
2020-07-11 23:10:04 +0800

    Update 1049. 数列的片段和(20).cpp

--- BasicLevel_C++/1049. 数列的片段和(20).cpp
+++ BasicLevel_C++/1049. 数列的片段和(20).cpp
@@ -3,11 +3,12 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    double sum = 0.0, temp;
+    long long sum = 0;
+    double temp;
     for (int i = 1; i <= n; i++) { 
         cin >> temp;
-        sum = sum + temp * i * (n - i + 1);
+        sum += (long long)(temp * 1000) * i * (n - i + 1);
     }
-    printf("%.2f", sum);
+    printf("%.2f", sum / 1000.0);
     return 0;
 }
