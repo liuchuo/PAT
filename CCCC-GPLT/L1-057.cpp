624218b1e79911adfb3c671f06630b9e4fd0b92b
liuchuo <95323362@qq.com>
2021-07-16 20:01:50 +0800

    -

---
+++ CCCC-GPLT/L1-057.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "PTA shi3 wo3 jing1 shen2 huan4 fa1 !";
+    return 0;
+}
---
+++ CCCC-GPLT/L1-061.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+float kg, m, res;
+int main() {
+    scanf("%f %f", &kg, &m);
+    res = kg / (m * m);
+    printf("%.1f\n", res);
+    if(res > 25.0) printf("PANG");
+    else printf("Hai Xing");
+	return 0;
+}
