11b12cc1a990dd0714a947d3fe32773c2a0e445a
liuchuo <11920560+liuchuo@users.noreply.github.com>
2022-08-18 15:33:56 +0800

    Update 1085. Perfect Sequence (25).cpp

--- AdvancedLevel_C++/1085. Perfect Sequence (25).cpp
+++ AdvancedLevel_C++/1085. Perfect Sequence (25).cpp
@@ -25,3 +25,27 @@ int main() {
     cout << result;
     return 0;
 }
+
+#include <iostream>
+#include <algorithm>
+#include <stdlib.h>
+using namespace std;
+int main() {
+    int n;
+    long long p;
+    cin >> n >> p;
+    if (n == 0) {
+        cout << n;
+        return 0;
+    }
+    long long int *a = new long long int[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a + n);
+    int result = 1;
+    for (int i = 0; i < n; i++) {
+        result = max((int)(upper_bound(a, a+n, a[i] * p) - (a+i)), result);
+    }
+    cout << result;
+    return 0;
+}
